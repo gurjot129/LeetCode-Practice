cc024d766805fe737c130eabe089bb4a65e5d5f6
Gurjot Saini <gurjot.saini129@gmail.com>
2024-09-18 18:45:32 -0400

    The following files have set up the problem

--- Top Interview 150/01. Array and String/1. Merge Sorted Array/Java/Merge Sorted Array/src/Main.java
+++ Top Interview 150/01. Array and String/1. Merge Sorted Array/Java/Merge Sorted Array/src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        //Test Case 1
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int m = 3;
+        int[] nums2 = { 2, 5, 6 };
+        int n = 3;
+
+
+        MergeSortedArray msa = new MergeSortedArray();
+        msa.merge(nums1, m,  nums2, n);
+
+        System.out.println("End of Main Function.");
     }
 }
---
+++ Top Interview 150/01. Array and String/1. Merge Sorted Array/Java/Merge Sorted Array/src/MergeSortedArray.java
@@ -0,0 +1,14 @@
+public class MergeSortedArray {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        //keeps track of which elements we are comparing
+        int nums1Idx = nums1.length - 1;
+        int num21Idx = nums2.length - 1;
+
+
+
+
+        System.out.println("End of Merge Function.");
+    }
+}
