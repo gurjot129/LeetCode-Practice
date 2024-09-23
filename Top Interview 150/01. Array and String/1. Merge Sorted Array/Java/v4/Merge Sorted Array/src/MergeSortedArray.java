9a7f477a0c07a49e1fde71b612079fa44917aa50
Gurjot Saini <gurjot.saini129@gmail.com>
2024-09-22 23:22:31 -0400

    Starts merge sorted array prgblem with a clean slate

--- Top Interview 150/01. Array and String/1. Merge Sorted Array/Java/v4/Merge Sorted Array/src/MergeSortedArray.java
+++ Top Interview 150/01. Array and String/1. Merge Sorted Array/Java/v4/Merge Sorted Array/src/MergeSortedArray.java
@@ -2,43 +2,21 @@ public class MergeSortedArray {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        //keeps track of which elements we are comparing
-        int nums1Idx = Math.max(m - 1, 0);
-        int nums2Idx = nums2.length - 1;
-        int currIdx = nums1.length - 1;
+        //prints nums1 array before modification step
+        //System.out.println("currIdx: " + currIdx);
+        System.out.print("nums1 before: ");
+        printArray(nums1);
+        System.out.print("nums2 before: ");
+        printArray(nums2);
 
-        //checks if nums2 is empty
-        if(nums2.length != 0) {
+        //prints nums1 array after modification step
+        System.out.print("nums1 after: ");
+        printArray(nums1);
+        System.out.print("nums2 after: ");
+        printArray(nums2);
+        System.out.println(" ");
 
-            //helps with sorting nums1 array
-            while(currIdx >= 0) {
-
-                System.out.println("currIdx: " + currIdx);
-                System.out.print("nums1 before: ");
-                printArray(nums1);
-                System.out.print("nums2 before: ");
-                printArray(nums2);
-
-                //decides which element is larger from each of the arrays, largest one is then inserted at the current index
-                if(nums2Idx >= 0 && nums2[nums2Idx] >= nums1[nums1Idx]) {
-                    nums1[currIdx] = nums2[nums2Idx];
-                    --nums2Idx;
-                    --currIdx;
-                } else {
-                    nums1[currIdx] = nums1[nums1Idx];
-                    --nums1Idx;
-                    --currIdx;
-                }
-
-                System.out.print("nums1 after: ");
-                printArray(nums1);
-                System.out.print("nums2 after: ");
-                printArray(nums2);
-                System.out.println(" ");
-
-            }
-        }
-
+        //prints final results
         System.out.println("Final nums1 result: ");
         printArray(nums1);
         System.out.println(" ");
