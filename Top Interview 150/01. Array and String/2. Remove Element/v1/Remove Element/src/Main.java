381115842a5f500929ce595122496b0942136ba2
Gurjot Saini <gurjot.saini129@gmail.com>
2024-09-25 19:44:56 -0400

    Final cleanup

--- Top Interview 150/01. Array and String/2. Remove Element/v1/Remove Element/src/Main.java
+++ Top Interview 150/01. Array and String/2. Remove Element/v1/Remove Element/src/Main.java
@@ -4,8 +4,16 @@ public class Main {
         RemoveElement re = new RemoveElement();
 
         //test case #1
-        int[] nums = { 3,2,2,3 };
-        int val = 3;
+//        int[] nums = { 3,2,2,3 };
+//        int val = 3;
+
+        //test case #2
+//        int[] nums = { 0,1,2,2,3,0,4,2 };
+//        int val = 2;
+
+        //test case #43
+        int[] nums = { 2,2,3 };
+        int val = 4;
 
         int k = re.removeElement(nums, val);
     }
