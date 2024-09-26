3676b28b63a6c7da41280473271fdcdcbded351a
Gurjot Saini <gurjot.saini129@gmail.com>
2024-09-25 20:15:45 -0400

    This code implementation passes ALL test cases

--- Top Interview 150/01. Array and String/2. Remove Element/v2/Remove Element/src/RemoveElement.java
+++ Top Interview 150/01. Array and String/2. Remove Element/v2/Remove Element/src/RemoveElement.java
@@ -5,6 +5,38 @@ public class RemoveElement {
         //creating and initializing variables
         int k = 0;
 
+        //ensures that the array isn't empty
+        if(nums.length != 0) {
+
+            //iterates through nums array in order to remove unwanted values and keep track of the remaining values
+            for(int i = 0; i < nums.length; ++i) {
+
+                System.out.println("i: " + i);
+                System.out.print("nums before: ");
+                printArray(nums);
+                System.out.println("k before: " + k);
+                System.out.println(" ");
+
+                //this logic analyzes array, decides which elements to remove or keep, and it moves remaining the beginning of the array,
+                if(nums[i] == val) {
+                    nums[i] = -1;
+                } else {
+                    nums[k] = nums[i];
+                    ++k;
+                }
+
+                System.out.print("nums after: ");
+                printArray(nums);
+                System.out.println("k after: " + k);
+                System.out.println(" ");
+            }
+        }
+
+        System.out.println("Final Results");
+        System.out.print("nums: ");
+        printArray(nums);
+        System.out.println("k: " + k);
+
         return k;
     }
 
