72002efa4386498d9d1152d32ec978f37e28d570
Gurjot Saini <gurjot.saini129@gmail.com>
2024-09-26 13:31:19 -0400

    This code implementation passes ALL test cases for the Remove Duplicates from Sorted Array problem

--- Top Interview 150/01. Array and String/3. Remove Duplicates from Sorted Array/v1/Remove Duplicates from Sorted Array/src/Main.java
+++ Top Interview 150/01. Array and String/3. Remove Duplicates from Sorted Array/v1/Remove Duplicates from Sorted Array/src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        RemoveDuplicatesfromSortedArray rdsa = new RemoveDuplicatesfromSortedArray();
+
+        //test case #2
+        int[] nums = { 0,0,1,1,1,2,2,3,3,4 };
+
+        int k = rdsa.removeDuplicates(nums);
+        System.out.println("Final Results:");
+        System.out.println("k: " + k);
+        System.out.println("nums: " + Arrays.toString(nums));
+
     }
 }
--- Top Interview 150/01. Array and String/3. Remove Duplicates from Sorted Array/v1/Remove Duplicates from Sorted Array/src/RemoveDuplicatesfromSortedArray.java
+++ Top Interview 150/01. Array and String/3. Remove Duplicates from Sorted Array/v1/Remove Duplicates from Sorted Array/src/RemoveDuplicatesfromSortedArray.java
@@ -1,4 +1,30 @@
 public class RemoveDuplicatesfromSortedArray {
 
+    public int removeDuplicates(int[] nums) {
 
+        //create and initializes variables
+        int k = 0;
+
+        //ensures that the array isn't empty
+        if(nums.length > 0) {
+
+            //we start by analyzing the first element, and it also ensures to handle 1 element array use case
+            int currVal = nums[0];
+            k = 1;
+
+            //if we have an array with more than one element, we begin to iterate through and analyze the other elements
+            for(int i = 1; i < nums.length; ++i) {
+
+                //if the element doesn't match the current value, then we count it as unique
+                //We also shift the unique values towards the left side of the array
+                if(nums[i] != currVal) {
+                    currVal = nums[i];
+                    nums[k] = nums[i];
+                    ++k;
+                }
+            }
+        }
+
+        return k;
+    }
 }
