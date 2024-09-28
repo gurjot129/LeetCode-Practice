10a013ea2fc780fdcd61a4fe8c775195a462c2cc
Gurjot Saini <gurjot.saini129@gmail.com>
2024-09-28 13:56:43 -0400

    Adds comments to code

--- Top Interview 150/01. Array and String/5. Majority Element/v1/Majority Element/src/Main.java
+++ Top Interview 150/01. Array and String/5. Majority Element/v1/Majority Element/src/Main.java
@@ -2,7 +2,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        Solution s = new Solution();
 
+        //test case #2
+        int[] nums = { 2,2,1,1,1,2,2 };
 
+        int k = s.majorityElement(nums);
+
+        System.out.println("Final Results:");
+        System.out.println("k: " + k);
     }
 }
