b3f8d9c43d8cc505bf16de6b4bb2ff14573af9c1
Gurjot Saini <gurjot.saini129@gmail.com>
2024-09-27 21:25:49 -0400

    This code implementation passes ALL use cases for the Remove Duplicates from Sorted Array II problem

--- Top Interview 150/01. Array and String/4. Remove Duplicates from Sorted Array II/v1/Remove Duplicates from Sorted Array II/src/Main.java
+++ Top Interview 150/01. Array and String/4. Remove Duplicates from Sorted Array II/v1/Remove Duplicates from Sorted Array II/src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        //test case #2
+        int[] nums = { 0,0,1,1,1,1,2,3,3 };
+
+        int k = s.removeDuplicates(nums);
+        System.out.println("Final Results:");
+        System.out.println("k: " + k);
+        System.out.println("nums: " + Arrays.toString(nums));
 
     }
 }
--- Top Interview 150/01. Array and String/4. Remove Duplicates from Sorted Array II/v1/Remove Duplicates from Sorted Array II/src/Solution.java
+++ Top Interview 150/01. Array and String/4. Remove Duplicates from Sorted Array II/v1/Remove Duplicates from Sorted Array II/src/Solution.java
@@ -1,4 +1,43 @@
 public class Solution {
 
-    
+    public int removeDuplicates(int[] nums) {
+
+        //create and initializes variables
+        int k = 0;
+
+        //ensures that the array isn't empty
+        if(nums.length > 0) {
+
+            //we start by analyzing the first element, and it also ensures to handle 1 element array use case
+            int currVal = nums[0];
+            int dupCnt = 0;
+            k = 1;
+
+            //if we have an array with more than one element, we begin to iterate through and analyze the other elements
+            for(int i = 1; i < nums.length; ++i) {
+
+                //if the element doesn't match the current value, then it is counted as unique
+                //if the element does match the current value and isn't duplicated more than once, then it is counted as unique
+                //it also shifts the unique values towards the left side of the array
+                if(nums[i] == currVal) {
+
+                    //a value is considered unique if it doesn't contain more than 1 duplicates
+                    if(dupCnt == 0) {
+                        nums[k] = nums[i];
+                        ++k;
+                    }
+                    ++dupCnt;
+
+                } else {
+                    currVal = nums[i];
+                    nums[k] = nums[i];
+                    dupCnt = 0;
+                    ++k;
+                }
+
+            }
+        }
+
+        return k;
+    }
 }
