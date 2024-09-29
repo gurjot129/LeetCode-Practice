bac1a8b9db673bf7d28f3807a466f2a6684cece7
Gurjot Saini <gurjot.saini129@gmail.com>
2024-09-28 21:14:47 -0400

    This code immplementation passes ALL test cases for the Rotate Array problem

--- Top Interview 150/01. Array and String/6. Rotate Array/v1/Rotate Array/src/Solution.java
+++ Top Interview 150/01. Array and String/6. Rotate Array/v1/Rotate Array/src/Solution.java
@@ -1,7 +1,23 @@
+import java.util.Arrays;
+
 class Solution {
     public void rotate(int[] nums, int k) {
 
-        
+        //creates and initializes variables
+        int[] temp = new int[nums.length];
 
+        //rotates the array elements to the right by the number of k amounts
+        for(int i = 0; i < nums.length; ++i) {
+            temp[(i + k) % nums.length] = nums[i];
+        }
+
+        //assigns the rotated elements to the nums array
+        for(int i = 0; i < nums.length; ++i) {
+            nums[i] = temp[i];
+        }
+
+        System.out.println("After assigning temp array to nums array");
+        System.out.println("temp: " + Arrays.toString(temp));
+        System.out.println("nums: " + Arrays.toString(nums));
     }
 }
