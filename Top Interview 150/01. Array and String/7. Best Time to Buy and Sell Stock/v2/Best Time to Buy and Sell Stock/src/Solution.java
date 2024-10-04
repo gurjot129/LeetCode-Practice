1767808be39fa77f3e7a0c381f3d4ce52ab67eb6
Gurjot Saini <gurjot.saini129@gmail.com>
2024-10-03 22:35:46 -0400

    This code implementation passes ALL test cases for the Buy and Sell stock problem for solution v2

--- Top Interview 150/01. Array and String/7. Best Time to Buy and Sell Stock/v2/Best Time to Buy and Sell Stock/src/Solution.java
+++ Top Interview 150/01. Array and String/7. Best Time to Buy and Sell Stock/v2/Best Time to Buy and Sell Stock/src/Solution.java
@@ -2,8 +2,18 @@ public class Solution {
 
     public int maxProfit(int[] prices) {
 
+        int maxProfit = 0;
+        int buy = Integer.MAX_VALUE;
 
+        for(int i = 0; i < prices.length; ++i) {
 
-        return 0;
+            if(prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] > maxProfit) {
+                maxProfit = Math.max(prices[i] - buy, maxProfit);
+            }
+        }
+
+        return maxProfit;
     }
 }
