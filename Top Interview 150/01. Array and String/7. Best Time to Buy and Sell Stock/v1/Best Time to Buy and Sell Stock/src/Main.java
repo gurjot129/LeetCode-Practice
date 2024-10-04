8647adaed94665386a6e66f3ddee9bacd0c5ddb8
Gurjot Saini <gurjot.saini129@gmail.com>
2024-10-03 21:53:51 -0400

    Adds test case #179, however it cannot be solved, the code needs to be re-worked

--- Top Interview 150/01. Array and String/7. Best Time to Buy and Sell Stock/v1/Best Time to Buy and Sell Stock/src/Main.java
+++ Top Interview 150/01. Array and String/7. Best Time to Buy and Sell Stock/v1/Best Time to Buy and Sell Stock/src/Main.java
@@ -7,7 +7,10 @@ public class Main {
 //        int[] prices = { 7,1,5,3,6,4 };
 
         //test case #155
-        int[] prices = { 2,4,1 };
+//        int[] prices = { 2,4,1 };
+
+        //test case #179
+        int[] prices = { 3,2,6,5,0,3 };
 
         int profit = s.maxProfit(prices);
 
