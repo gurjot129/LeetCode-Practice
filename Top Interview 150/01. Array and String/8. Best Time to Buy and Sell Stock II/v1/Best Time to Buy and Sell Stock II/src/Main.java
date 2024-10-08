5ebe3fb0876a5c80a9c566731a2c72398107698e
Gurjot Saini <gurjot.saini129@gmail.com>
2024-10-08 18:35:11 -0400

    This code implementation passes test cases #1 and #3 for the Buy Sell Stock II problem

--- Top Interview 150/01. Array and String/8. Best Time to Buy and Sell Stock II/v1/Best Time to Buy and Sell Stock II/src/Main.java
+++ Top Interview 150/01. Array and String/8. Best Time to Buy and Sell Stock II/v1/Best Time to Buy and Sell Stock II/src/Main.java
@@ -8,10 +8,10 @@ public class Main {
 //        int[] prices = { 7,1,5,3,6,4 };
 
         //test case #2
-//        int[] prices = { 1,2,3,4,5 };
+        int[] prices = { 1,2,3,4,5 };
 
         //test case #3
-        int[] prices = { 7,6,4,3,1 };
+//        int[] prices = { 7,6,4,3,1 };
 
         int profit = s.maxProfit(prices);
 
--- Top Interview 150/01. Array and String/8. Best Time to Buy and Sell Stock II/v1/Best Time to Buy and Sell Stock II/src/Solution.java
+++ Top Interview 150/01. Array and String/8. Best Time to Buy and Sell Stock II/v1/Best Time to Buy and Sell Stock II/src/Solution.java
@@ -1,7 +1,38 @@
+import jdk.jshell.spi.ExecutionControl;
+
 public class Solution {
 
     public int maxProfit(int[] prices) {
 
-        return 0;
+        //creates and initializes variables
+        int buyIdx = 0;
+        int sellIdx = 1;
+        int maxSellIdx = -1;
+        int maxProfit = 0;
+        int sellCount = 0;
+
+        //iterates through the prices array
+        while(sellIdx < prices.length && sellCount < 2) {
+
+            if(prices[buyIdx] < prices[sellIdx]) {
+
+                if(maxSellIdx != -1) {
+                    maxProfit += Math.max(prices[sellIdx] - prices[buyIdx], prices[maxSellIdx] - prices[buyIdx]);
+                    buyIdx = sellIdx;
+                    maxSellIdx = -1;
+                    ++sellCount;
+                } else {
+                    maxSellIdx = sellIdx;
+                }
+            }
+            else {
+                buyIdx = sellIdx;
+            }
+
+            //ensures that we evaluate every sell price regardless of conditions above
+            ++sellIdx;
+        }
+
+        return maxProfit;
     }
 }
