58343bbb22e60f0faba91085fcc9419bd4f9bd78
Gurjot Saini <gurjot.saini129@gmail.com>
2024-10-09 18:57:46 -0400

    This code implementation passes ALL test cases for the Jump Game problem for v3 solution

--- Top Interview 150/01. Array and String/9. Jump Game/v3/Jump Game/src/Solution.java
+++ Top Interview 150/01. Array and String/9. Jump Game/v3/Jump Game/src/Solution.java
@@ -1,7 +1,19 @@
 public class Solution {
 
+    //this solution uses the Greedy Method to solve this problem
     public boolean canJump(int[] nums) {
 
-        return true;
+        //starts from the last index
+        int goal = nums.length - 1;
+
+        //this code works its way backwards from the last to the first index
+        for(int i = nums.length - 1; i >= 0; --i) {
+
+            //if there are sufficient jumps to reach the goal, then move the goal closer to the first index
+            if((i + nums[i]) >= goal)
+                goal = i;
+        }
+
+        return goal == 0;
     }
 }
