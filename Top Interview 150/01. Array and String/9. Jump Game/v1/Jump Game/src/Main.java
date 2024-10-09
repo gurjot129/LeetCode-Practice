9bfc0bd7fc41c035b11ee6a6ca1f7eff647cacdc
Gurjot Saini <gurjot.saini129@gmail.com>
2024-10-09 01:40:04 -0400

    This code implementation passes 157/173 test cases for the Jump Game problem for v1 solution, however, it does not pass test case #174

--- Top Interview 150/01. Array and String/9. Jump Game/v1/Jump Game/src/Main.java
+++ Top Interview 150/01. Array and String/9. Jump Game/v1/Jump Game/src/Main.java
@@ -4,7 +4,10 @@ public class Main {
         Solution s = new Solution();
 
         //test case #1
-        int[] nums = { 2,3,1,1,4 };
+//        int[] nums = { 2,3,1,1,4 };
+
+        //test case #2
+        int[] nums = { 3,2,1,0,4 };
 
         boolean canJump = s.canJump(nums);
 
--- Top Interview 150/01. Array and String/9. Jump Game/v1/Jump Game/src/Solution.java
+++ Top Interview 150/01. Array and String/9. Jump Game/v1/Jump Game/src/Solution.java
@@ -2,6 +2,14 @@ public class Solution {
 
     public boolean canJump(int[] nums) {
 
-        return true;
+        //creates and initializes variables
+        int jumps = 0;
+
+        while(jumps < (nums.length - 1) && nums[jumps] != 0) {
+            //int max = nums[jumps] != 0? 1 + nums[jumps + 1] : 0;
+            jumps += nums[jumps];
+        }
+
+        return jumps >= (nums.length - 1);
     }
 }
