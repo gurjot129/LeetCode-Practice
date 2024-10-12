f3c327945ef7c6629aabfdf88dfe13370ba4e4c0
Gurjot Saini <gurjot.saini129@gmail.com>
2024-10-12 02:17:31 -0400

    Just to clarify, the previous commit had more than just boilerplate code, anyways, this code implementation passes test case #1 and #2

--- Top Interview 150/01. Array and String/11. H-Index/v1/H-Index/src/Solution.java
+++ Top Interview 150/01. Array and String/11. H-Index/v1/H-Index/src/Solution.java
@@ -1,21 +1,23 @@
-import java.util.Arrays;
-
 public class Solution {
 
     public int hIndex(int[] citations) {
 
+        //creates and initializes variables
         int hIndex = 0;
 
-        Arrays.sort(citations);
-
+        //this iterates through the number of papers
         for(int i = 1; i <= citations.length; ++i) {
 
+            //counts how many minimum number of citations per paper match equal or greater to the number of papers
             int citPapEq = 0;
 
+            //evaluates the number of citations per paper in order to identity which
+            //papers' citations match equal or greater to the number of papers
             for(int j = 0; j < citations.length && citPapEq != i; ++j) {
                 if(citations[j] >= i) ++citPapEq;
             }
 
+            //if the required number of papers is met, increment H-Index value
             if(citPapEq == i) ++hIndex;
         }
 
