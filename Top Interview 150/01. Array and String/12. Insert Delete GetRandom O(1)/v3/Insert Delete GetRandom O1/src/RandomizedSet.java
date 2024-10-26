c3855bd0ab40cfb551dbf32535a2217ed00c659a
Gurjot Saini <gurjot.saini129@gmail.com>
2024-10-26 17:00:04 -0400

    This code implementation passes ALL test cases for the Insert Delete GetRandom O1 problem with v3 solution

--- Top Interview 150/01. Array and String/12. Insert Delete GetRandom O(1)/v3/Insert Delete GetRandom O1/src/RandomizedSet.java
+++ Top Interview 150/01. Array and String/12. Insert Delete GetRandom O(1)/v3/Insert Delete GetRandom O1/src/RandomizedSet.java
@@ -1,29 +1,63 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 class RandomizedSet {
+
+    /*
+     * Overall, HashMap helps with finding values (during insert and remove operations),
+     * ArrayList helps with the random operation
+     */
 
     //creates and initializes variables
     private ArrayList<Integer> items;
     private HashMap<Integer, Integer> hashItems;
     private Random random;
 
+    //constructor, initializes the RandomizedSet object
     public RandomizedSet() {
 
+        items = new ArrayList<>();
+        hashItems = new HashMap<>();
+        random = new Random();
     }
+
+    //inserts an item val into the set if not present. Returns true if the item was not present, false otherwise
     public boolean insert(int val) {
 
+        if(hashItems.containsKey(val)) {
+            return false;
+        }
+
+        hashItems.put(val, items.size());
+        items.add(val);
+
         return true;
     }
+
+    //removes an item val from the set if present. Returns true if the item was present, false otherwise
     public boolean remove(int val) {
 
+        if(!hashItems.containsKey(val)) {
+            return false;
+        }
+
+        int valIdx = hashItems.get(val);
+        int lastVal = items.getLast();
+
+        items.set(valIdx, lastVal);
+        items.removeLast();
+
+        hashItems.replace(lastVal, valIdx);
+        hashItems.remove(val);
+
         return true;
     }
+
+    //removes an item val from the set if present. Returns true if the item was present, false otherwise
     public int getRandom() {
 
-        return 0;
+        return items.get(random.nextInt(items.size()));
     }
 }
 
