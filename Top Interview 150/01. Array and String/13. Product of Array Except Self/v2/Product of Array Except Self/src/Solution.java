53ae151a4f9de291ec67387f472b07bf3286bd63
Gurjot Saini <gurjot.saini129@gmail.com>
2024-10-27 01:56:18 -0400

    This code implementation passes 18/24 test cases for the Product of Array Except Self problem with v2 solution, however, test case #19 does not pass, the two pointer method was used this time to reduce execution time, however, time limit still exceeded

--- Top Interview 150/01. Array and String/13. Product of Array Except Self/v2/Product of Array Except Self/src/Solution.java
+++ Top Interview 150/01. Array and String/13. Product of Array Except Self/v2/Product of Array Except Self/src/Solution.java
@@ -5,7 +5,25 @@ class Solution {
         //create and initialize variables
         int[] answer = new int[nums.length];
 
-        
+        //initializes all answer values to 1
+        for(int i = 0; i < nums.length; ++i) {
+            answer[i] = 1;
+        }
+
+        //starts at the second element, since we are not multiplying all values to each other
+        for(int i = 1; i <= (nums.length / 2); ++i) {
+
+            //multiplies values for each element, where i helps shift right and left of the j value that need to be multiplied to j
+            for(int j = 0; j < nums.length; ++j) {
+
+                if(((j + i) % nums.length) != (((j - i) < 0 ? nums.length + (j - i) : j - i) % nums.length) ) {
+                    answer[j] = answer[j] * (nums[(j + i) % nums.length]) * (nums[(((j - i) < 0 ? nums.length + (j - i) : j - i) % nums.length)]);
+                } else {
+                    answer[j] = answer[j] * (nums[(j + i) % nums.length]);
+                }
+
+            }
+        }
 
         return answer;
     }
