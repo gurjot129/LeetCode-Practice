e2e69faf62ca102a0e8eabba61ab46d5a1ea21c6
Gurjot Saini <gurjot.saini129@gmail.com>
2024-10-26 20:31:46 -0400

    This code implementation passes 18/24 test cases for the Product of Array Except Self problem with v1 solution, however, test case #15 does not pass, since it exceeds the time limit

--- Top Interview 150/01. Array and String/13. Product of Array Except Self/v1/Product of Array Except Self/src/Main.java
+++ Top Interview 150/01. Array and String/13. Product of Array Except Self/v1/Product of Array Except Self/src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        Solution s = new Solution();
 
+        //test case #1
+//        int[] nums = { 1,2,3,4 };
+
+        //test case #2
+        int[] nums = { -1,1,0,-3,3 };
+
+        int[] answer = s.productExceptSelf(nums);
+
+        System.out.println("Final Results:");
+        System.out.println("answer: " + Arrays.toString(answer));
 
     }
 }
--- Top Interview 150/01. Array and String/13. Product of Array Except Self/v1/Product of Array Except Self/src/Solution.java
+++ Top Interview 150/01. Array and String/13. Product of Array Except Self/v1/Product of Array Except Self/src/Solution.java
@@ -5,7 +5,19 @@ class Solution {
         //create and initialize variables
         int[] answer = new int[nums.length];
 
-        
+        //initializes all answer values to 1
+        for(int i = 0; i < nums.length; ++i) {
+            answer[i] = 1;
+        }
+
+        //starts at the second element, since we are not multiplying all values to each other
+        for(int i = 1; i < nums.length; ++i) {
+
+            //multiplies values for each element, where i helps shift to the values that need to be multiplied
+            for(int j = 0; j < nums.length; ++j) {
+                answer[j] = answer[j] * nums[(i + j) % nums.length];
+            }
+        }
 
         return answer;
     }
