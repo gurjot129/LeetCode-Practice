2c0ccffc3e334e2825618f186671026319ea9b36
Gurjot Saini <gurjot.saini129@gmail.com>
2024-10-30 23:57:45 -0400

    This code implementation passes ALL test cases for the Product of Array Except Self problem with v3 solution, needed to create a prefix and suffix calculations in order to run efficiently, and no additional arrays required

--- Top Interview 150/01. Array and String/13. Product of Array Except Self/v3/Product of Array Except Self/src/Solution.java
+++ Top Interview 150/01. Array and String/13. Product of Array Except Self/v3/Product of Array Except Self/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
 
     public int[] productExceptSelf(int[] nums) {
@@ -5,7 +7,21 @@ class Solution {
         //create and initialize variables
         int[] answer = new int[nums.length];
 
-        
+        int prefix = 1;
+        int suffix = 1;
+
+        //this code calculates and stores the prefix values in the answer array
+        for(int i = 0; i < nums.length; ++i) {
+            answer[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        //this code calculates and stores the final product values (prefix & suffix) in the answer array
+        for(int i = nums.length - 1; i >= 0; --i) {
+            answer[i] *= suffix;
+            suffix *= nums[i];
+
+        }
 
         return answer;
     }
