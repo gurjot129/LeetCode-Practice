72c62f03c985d0cfabb13c0f6d0c840f5684043f
Gurjot Saini <gurjot.saini129@gmail.com>
2024-11-01 19:24:45 -0400

    This code implementation passes ALL test cases for the Gas Station problem with v2 solution

--- Top Interview 150/01. Array and String/14. Gas Station/v2/Gas Station/src/Solution.java
+++ Top Interview 150/01. Array and String/14. Gas Station/v2/Gas Station/src/Solution.java
@@ -2,6 +2,30 @@ public class Solution {
 
     public int canCompleteCircuit(int[] gas, int[] cost) {
 
-        return 0;
+        //car's gas tank
+        int gasTank = 0;
+
+        //totals all the gas and cost elements to quickly check if the car is able to travel around the circuit
+        for(int i = 0; i < gas.length; ++i) gasTank += gas[i] - cost[i];
+
+        //if car's gas tank falls below zero, there is no solution that allows it to travel around the circuit
+        if(gasTank < 0) return -1;
+
+        int startGasStn = 0;
+        gasTank = 0;
+
+        //this calculates which gas station allows the car to travel around the circuit
+        for(int i = 0; i < gas.length; ++i) {
+
+            gasTank += gas[i] - cost[i];
+
+            //if gas tank is below zero, the next gas station may have a better start and allow the car to travel around the circuit
+            if(gasTank < 0) {
+                gasTank = 0;
+                startGasStn = i + 1;
+            }
+        }
+
+        return startGasStn;
     }
 }
