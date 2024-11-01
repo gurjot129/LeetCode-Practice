f61833e37ab708134fb954ba6cb11a7c41995a69
Gurjot Saini <gurjot.saini129@gmail.com>
2024-10-31 20:18:50 -0400

    This code implementation passes 17/39 test cases for the Gas Station problem with v1 solution, however, it does not pass test case #18

--- Top Interview 150/01. Array and String/14. Gas Station/v1/Gas Station/src/Main.java
+++ Top Interview 150/01. Array and String/14. Gas Station/v1/Gas Station/src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        Solution s = new Solution();
 
+        //test case #1
+        int[] gas = { 1,2,3,4,5 };
+        int[] cost = { 3,4,5,1,2 };
+
+        int startGasStn = s.canCompleteCircuit(gas, cost);
+
+        System.out.println("Final Results:");
+        System.out.println("Starting Gas Station: " + startGasStn);
 
     }
 }
--- Top Interview 150/01. Array and String/14. Gas Station/v1/Gas Station/src/Solution.java
+++ Top Interview 150/01. Array and String/14. Gas Station/v1/Gas Station/src/Solution.java
@@ -1,8 +1,28 @@
 public class Solution {
 
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        
-        return 0;
+
+        //this code implementation iterates through each valid gas station
+        for(int i = 0; i < gas.length - 1; ++i) {
+
+            //car starts from empty tank
+            int gasTank = 0;
+
+            //starts at the ith gas station, calculates and checks if the car is able to travel around the circuit
+            for(int j = 0; j < gas.length - 1; ++j) {
+                gasTank += gas[(i + j) % (gas.length)];
+                gasTank -= cost[(i + j) % (gas.length)];
+
+                //if the car is out of gas, start again but at the next gas station
+                if(gasTank <= 0) break;
+            }
+
+            //if the car is able to travel around the circuit, return starting ith gas station
+            if(gasTank >= 0) return i;
+        }
+
+        //if no starting gas station is able to travel around the circuit, return -1
+        return -1;
     }
 
 }
