34568e2af774338f947eadf5cc1c55b5fd220d99
Gurjot Saini <gurjot.saini129@gmail.com>
2024-11-04 18:10:09 -0500

    These files and folders contain the information, project and boilerplate code for the Candy problem for v2 solution

---
+++ Top Interview 150/01. Array and String/15. Candy/v2/Candy/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        //test case #1
+//        int[] ratings = { 1,0,2 };
+
+        //test case #2
+        int[] ratings = { 1,2,2 };
+
+        int minCandies = s.candy(ratings);
+
+        System.out.println("Final Results:");
+        System.out.println("Minimum Candies: " + minCandies);
+
+    }
+}
---
+++ Top Interview 150/01. Array and String/15. Candy/v2/Candy/src/Solution.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Solution {
+
+    public int candy(int[] ratings) {
+
+        return 0;
+    }
+}
