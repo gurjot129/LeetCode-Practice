d7cbbfa9b1fccbf59cdc360bf89f41c3ac36b67b
Gurjot Saini <gurjot.saini129@gmail.com>
2024-11-04 18:55:30 -0500

    This code implementation passes ALL test cases for the Candy problem with v2 solution

--- Top Interview 150/01. Array and String/15. Candy/v2/Candy/src/Solution.java
+++ Top Interview 150/01. Array and String/15. Candy/v2/Candy/src/Solution.java
@@ -1,9 +1,26 @@
-import java.util.Arrays;
-
 public class Solution {
 
     public int candy(int[] ratings) {
 
-        return 0;
+        //creates and initializes variables
+        int[] candies = new int[ratings.length];
+        int candySum = 0;
+
+        //all children at least have 1 candy
+        for(int i = 0; i < candies.length; ++i)
+            candies[i] = 1;
+
+        //comparing ratings from left to right
+        for(int i = 1; i < ratings.length; ++i)
+            if(ratings[i - 1] < ratings[i]) candies[i] = candies[i - 1] + 1;
+
+        //comparing ratings from right to left
+        for(int i = ratings.length - 2; i >= 0; --i)
+            if(ratings[i] > ratings[i + 1])
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+
+        for(int i : candies) candySum += i;
+
+        return candySum;
     }
 }
