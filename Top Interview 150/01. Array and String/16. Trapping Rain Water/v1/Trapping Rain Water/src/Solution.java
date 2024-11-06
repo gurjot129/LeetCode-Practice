d15497b27840cd6a2355f192a9d8e91c2b8b4736
Gurjot Saini <gurjot.saini129@gmail.com>
2024-11-06 02:14:26 -0500

    This code implementation passes ALL test cases for the Trapping Rain Water problem with v1 solution

--- Top Interview 150/01. Array and String/16. Trapping Rain Water/v1/Trapping Rain Water/src/Solution.java
+++ Top Interview 150/01. Array and String/16. Trapping Rain Water/v1/Trapping Rain Water/src/Solution.java
@@ -2,6 +2,36 @@ class Solution {
 
     public int trap(int[] height) {
 
-        return 0;
+        //creates and initializes variables
+        int maxHeightIdx = 0;
+        int escapedWaterStopPointIdx = 0;
+        int[] trapRainWater = new int[height.length];
+        int trappedRainWater = 0;
+
+        //checks trapped water from the left to right
+        for(int i = 0; i < height.length; ++i) {
+
+            trapRainWater[i] = Math.max(height[maxHeightIdx] - height[i], 0);
+
+            if(height[i] > height[maxHeightIdx]) maxHeightIdx = i;
+        }
+
+        escapedWaterStopPointIdx = maxHeightIdx;
+        maxHeightIdx = height.length - 1;
+
+        //checks escaped water from the right to left
+        for(int i = height.length - 1; i >= escapedWaterStopPointIdx; --i) {
+
+            trapRainWater[i] = Math.max(height[maxHeightIdx] - height[i], 0);
+
+            if(height[i] > height[maxHeightIdx]) maxHeightIdx = i;
+        }
+
+        //sums up all the trapped water
+        for(int i = 1; i < trapRainWater.length - 1; ++i) {
+            trappedRainWater += trapRainWater[i];
+        }
+
+        return trappedRainWater;
     }
 }
