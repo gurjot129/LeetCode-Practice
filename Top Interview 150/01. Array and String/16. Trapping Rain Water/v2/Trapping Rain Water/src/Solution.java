e7d2f75d933253080c3b1a0d05d85554d81d914b
Gurjot Saini <gurjot.saini129@gmail.com>
2024-11-08 17:50:16 -0500

    This code implementation passes ALL test cases for the Trapping Rain Water problem with v2 solution

--- Top Interview 150/01. Array and String/16. Trapping Rain Water/v2/Trapping Rain Water/src/Solution.java
+++ Top Interview 150/01. Array and String/16. Trapping Rain Water/v2/Trapping Rain Water/src/Solution.java
@@ -2,6 +2,32 @@ class Solution {
 
     public int trap(int[] height) {
 
-        return 0;
+        //creates and initializes variables
+        int lIdx = 0;
+        int rIdx = height.length - 1;
+
+        int lMax = height[lIdx];
+        int rMax = height[rIdx];
+
+        int totalWater = 0;
+
+        //evaluates each element starting from the opposite sides, it will continue to evaluate until the pointers meet each other
+        while(lIdx < rIdx) {
+
+            //the pointer with the lower elevation will move, since we do not want to cross an elevation greater than the max
+            if(lMax < rMax) {
+
+                ++lIdx;
+                lMax = Math.max(lMax, height[lIdx]);
+                totalWater += lMax - height[lIdx];
+            } else {
+
+                --rIdx;
+                rMax = Math.max(rMax, height[rIdx]);
+                totalWater += rMax - height[rIdx];
+            }
+        }
+
+        return totalWater;
     }
 }
