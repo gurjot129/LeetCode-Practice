b8a47cf78f2f74a79c94401383d0f4592adbf315
Gurjot Saini <gurjot.saini129@gmail.com>
2024-11-10 01:24:35 -0500

    These files and folders contain the information, project and boilerplate code for the Length of Last Word problem for v1 solution

---
+++ Top Interview 150/01. Array and String/19. Length of Last Word/v1/Length of Last Word/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        //test case #1
+        String string = "Hello World";
+
+
+        int lastWordLength = s.lengthOfLastWord(string);
+
+        System.out.println("Final Results:");
+        System.out.println("Length of Last Word: " + lastWordLength);
+
+    }
+}
