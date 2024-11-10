145dc845de66c9221e60a43cbea96ca37d85b97b
Gurjot Saini <gurjot.saini129@gmail.com>
2024-11-09 23:53:54 -0500

    These files and folders contain the information, project and boilerplate code for the Integer to Roman problem for v1 solution

---
+++ Top Interview 150/01. Array and String/18. Integer to Roman/v1/Integer to Roman/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        //test case #1
+        int integer = 3749;
+
+        String roman = s.intToRoman(integer);
+
+        System.out.println("Final Results:");
+        System.out.println("Roman value: " + roman);
+
+
+    }
+}
