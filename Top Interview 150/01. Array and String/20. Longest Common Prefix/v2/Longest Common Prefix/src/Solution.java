eb0bb58c688f2a11bc0a4ba61aec27ca4f614b2f
Gurjot Saini <gurjot.saini129@gmail.com>
2024-11-12 17:12:49 -0500

    This code implementation passes ALL test cases for the Longest Common Prefix problem with v2 solution

--- Top Interview 150/01. Array and String/20. Longest Common Prefix/v2/Longest Common Prefix/src/Solution.java
+++ Top Interview 150/01. Array and String/20. Longest Common Prefix/v2/Longest Common Prefix/src/Solution.java
@@ -1,10 +1,29 @@
+import java.util.Arrays;
+
 public class Solution {
 
     public String longestCommonPrefix(String[] strs) {
 
         //creates and initializes variables
-        String lcp = "";
+        StringBuilder lcp = new StringBuilder();
 
-        return lcp;
+        //sorts array of strings lexicographically
+        Arrays.sort(strs);
+
+        //once sorted, only the first and last elements matter
+        String firstElement = strs[0];
+        String lastElement = strs[strs.length - 1];
+
+        //evaluates the first and last string only, and finds the longest common prefix
+        for(int i = 0; i < Math.min(firstElement.length(), lastElement.length()); ++i) {
+
+            //if the characters don't match at the same index, end the loop and return the characters that do match
+            if(firstElement.charAt(i) != lastElement.charAt(i))
+                return lcp.toString();
+
+            lcp.append(firstElement.charAt(i));
+        }
+
+        return lcp.toString();
     }
 }
