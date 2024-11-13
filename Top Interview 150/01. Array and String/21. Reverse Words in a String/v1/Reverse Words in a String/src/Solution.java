3dd4da93e30090ffd593474053588970e414343a
Gurjot Saini <gurjot.saini129@gmail.com>
2024-11-12 21:07:28 -0500

    This code implementation passes ALL test cases for the Reverse Words in a String problem with v1 solution

--- Top Interview 150/01. Array and String/21. Reverse Words in a String/v1/Reverse Words in a String/src/Solution.java
+++ Top Interview 150/01. Array and String/21. Reverse Words in a String/v1/Reverse Words in a String/src/Solution.java
@@ -3,10 +3,15 @@ public class Solution {
     public String reverseWords(String s) {
 
         //creates and initializes variables
+        // StringBuilder is more efficient to use since it is mutable (not creating new object everytime)
         StringBuilder reversedString = new StringBuilder();
+        //removes excess space from both ends of the string, and splits all the words into an array regardless of space in between
+        String[] sSplit = s.trim().split("\\s+");
 
-        
+        //appends the words in reverse
+        for(int i = sSplit.length - 1; i >= 0; --i)
+            reversedString.append(sSplit[i]).append(" ");
 
-        return reversedString.toString();
+        return reversedString.toString().trim();
     }
 }
