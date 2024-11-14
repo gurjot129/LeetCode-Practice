149160586ac54c71aaff6004826f510c7cb84045
Gurjot Saini <gurjot.saini129@gmail.com>
2024-11-13 21:06:57 -0500

    This code implementation passes ALL test cases for the Find the Index of the First Occurrence in a String problem with v1 solution

--- Top Interview 150/01. Array and String/23. Find the Index of the First Occurrence in a String/v1/Find the Index of the First Occurrence in a String/src/Solution.java
+++ Top Interview 150/01. Array and String/23. Find the Index of the First Occurrence in a String/v1/Find the Index of the First Occurrence in a String/src/Solution.java
@@ -2,7 +2,8 @@ public class Solution {
 
     public int strStr(String haystack, String needle) {
 
-        return 0;
+        //returns the index of the first occurrence of needle in haystack, if not found return -1
+        return haystack.indexOf(needle);
     }
 
 }
