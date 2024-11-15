1dc27c73bf85865a56eecc798cfe0e08aa36afa7
Gurjot Saini <gurjot.saini129@gmail.com>
2024-11-15 17:46:24 -0500

    These files and folders contain the information, project and boilerplate code for the Text Justification problem for v3 solution

---
+++ Top Interview 150/01. Array and String/24. Text Justification/v3/Text Justification/src/Solution.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    //creates and initializes object variables
+    private List<String> textJustification;
+
+    public Solution() {
+        textJustification = new ArrayList<>();
+    }
+
+    public List<String> fullJustify(String[] words, int maxWidth) {
+
+        return textJustification;
+    }
+}
