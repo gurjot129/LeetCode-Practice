f043f894b14cadd605489a40c8460b32d10e2ef0
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-13 19:18:24 -0500

    This code implementation passes 485/486 test cases for the Valid Palindrome problem with v1 solution, however, it does not pass test case #486 due to the underscore character not being removed, since it is not an alphanumeric character

--- Top Interview 150/02. Two Pointers/01. Valid Palindrome/v1/Valid Palindrome/src/Solution.java
+++ Top Interview 150/02. Two Pointers/01. Valid Palindrome/v1/Valid Palindrome/src/Solution.java
@@ -2,6 +2,14 @@ public class Solution {
 
     public boolean isPalindrome(String s) {
 
+        s = s.toLowerCase().replaceAll("[^\\w]", "");
+
+        System.out.println("After modifying string s: " + s);
+
+        for(int i = 0; i < s.length() / 2; ++i)
+            if(s.charAt(i) != s.charAt(s.length() - 1 - i))
+                return false;
+
         return true;
     }
 }
