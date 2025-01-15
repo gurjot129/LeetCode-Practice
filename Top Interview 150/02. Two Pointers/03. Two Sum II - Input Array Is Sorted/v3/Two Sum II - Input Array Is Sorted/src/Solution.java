c11f3cbe6ea09b6211d40491654962d4d0e50f33
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-15 17:46:33 -0500

    This code implementation passes ALL test cases for the Two Sum problem with v3 solution

--- Top Interview 150/02. Two Pointers/03. Two Sum II - Input Array Is Sorted/v3/Two Sum II - Input Array Is Sorted/src/Solution.java
+++ Top Interview 150/02. Two Pointers/03. Two Sum II - Input Array Is Sorted/v3/Two Sum II - Input Array Is Sorted/src/Solution.java
@@ -2,6 +2,21 @@ public class Solution {
 
     public int[] twoSum(int[] numbers, int target) {
 
-        return new int[]{-1, -1};
+        //creates and initializes variables
+        int lptr = 0;
+        int rPtr = numbers.length - 1;
+
+        //iterates through the numbers by moving from each end inwards until target is found
+        while(lptr < rPtr) {
+
+            if(numbers[lptr] + numbers[rPtr] < target)
+                ++lptr;
+            else if(numbers[lptr] + numbers[rPtr] > target)
+                --rPtr;
+            else
+                break;
+        }
+
+        return new int[]{lptr + 1, rPtr + 1};
     }
 }
