fb3bc3463dfe53481846dc98e5df003f0cd54eab
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-15 16:24:24 -0500

    These files and folders contain the information, project and boilerplate code for the Two Sum II problem for v1 solution

---
+++ Top Interview 150/02. Two Pointers/03. Two Sum II - Input Array Is Sorted/v1/Two Sum II - Input Array Is Sorted/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        //test case #1
+        int [] numbers = {2,7,11,15};
+        int target = 9;
+
+        int[] result = solution.twoSum(numbers, target);
+
+        System.out.println("*** FINAL RESULTS ***");
+        System.out.println("Two Sum Indices: ");
+        System.out.println(Arrays.toString(result));
+    }
+}
---
+++ Top Interview 150/02. Two Pointers/03. Two Sum II - Input Array Is Sorted/v1/Two Sum II - Input Array Is Sorted/src/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+
+    public int[] twoSum(int[] numbers, int target) {
+
+        //creates and initializes variables
+        int[] targetIdcs = new int[2];
+        
+        return targetIdcs;
+    }
+}
