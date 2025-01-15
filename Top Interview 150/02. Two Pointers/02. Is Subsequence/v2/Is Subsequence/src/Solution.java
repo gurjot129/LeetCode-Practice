8f4fbd90a9114c6923cb75071c231d421eb0361c
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-14 21:03:04 -0500

    This code implementation passes ALL test cases for the Is Subsequence problem with v2 solution

--- Top Interview 150/02. Two Pointers/02. Is Subsequence/v2/Is Subsequence/src/Solution.java
+++ Top Interview 150/02. Two Pointers/02. Is Subsequence/v2/Is Subsequence/src/Solution.java
@@ -2,6 +2,19 @@ public class Solution {
 
     public boolean isSubsequence(String s, String t) {
 
-        return true;
+        //creates and initializes variables
+        int sPtr = 0;
+        int tPtr = 0;
+
+        //iterates through each of the string character until we reach the end of the array for either string values
+        while(sPtr < s.length() && tPtr < t.length()) {
+
+            //if a character matches from both values, increment the s pointer
+            if(s.charAt(sPtr) == t.charAt(tPtr))  ++sPtr;
+
+            ++tPtr;
+        }
+
+        return sPtr == s.length();
     }
 }
