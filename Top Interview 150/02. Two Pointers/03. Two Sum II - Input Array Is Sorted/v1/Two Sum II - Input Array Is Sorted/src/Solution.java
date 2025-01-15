ec14eeac95bcc461067e1e765a2007029a4d4022
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-15 16:54:16 -0500

    This code implementation passes 1st test case for the Two Sum problem with v1 solution, however, it did not pass test casse #2 since the result array indices are reversed

--- Top Interview 150/02. Two Pointers/03. Two Sum II - Input Array Is Sorted/v1/Two Sum II - Input Array Is Sorted/src/Solution.java
+++ Top Interview 150/02. Two Pointers/03. Two Sum II - Input Array Is Sorted/v1/Two Sum II - Input Array Is Sorted/src/Solution.java
@@ -3,8 +3,33 @@ public class Solution {
     public int[] twoSum(int[] numbers, int target) {
 
         //creates and initializes variables
-        int[] targetIdcs = new int[2];
-        
-        return targetIdcs;
+        int lPtr = 0;
+
+        //iterates through the numbers array
+        while(lPtr < numbers.length) {
+
+            //creates a starting point for the right pointer one index ahead of the left pointer
+            //that way, we don't have to worry about skipping the left pointer index
+            int rPtr = (lPtr + 1) % numbers.length;
+
+            //iterates through the numbers array with the right pointer,
+            //comparing both left and right pointer values to the target
+            while(rPtr < numbers.length - 1) {
+
+                //if the summed pointer values match target, return indices with +1 to each index
+                if(numbers[lPtr] + numbers[rPtr] == target)
+                    return new int[]{lPtr + 1, rPtr + 1};
+
+                //this ensures that the right pointer doesn't go out of bounds
+                if (rPtr % numbers.length == numbers.length - 1)
+                    rPtr = 0;
+                else
+                    ++rPtr;
+            }
+
+            ++lPtr;
+        }
+
+        return new int[]{-1, -1};
     }
 }
