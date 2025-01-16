4ab386301e001c1afe8c7e290b4cad96634bed9f
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-16 18:16:05 -0500

    This code implementation passes ALL test cases for the Container with Most Water problem with v2 solution

--- Top Interview 150/02. Two Pointers/04. Container With Most Water/v2/Container With Most Water/src/Solution.java
+++ Top Interview 150/02. Two Pointers/04. Container With Most Water/v2/Container With Most Water/src/Solution.java
@@ -2,6 +2,26 @@ public class Solution {
 
     public int maxArea(int[] height) {
 
-        return 0;
+        //creates and initializes variables
+        int lPtr = 0;
+        int rPtr = height.length - 1;
+        int maxArea = 0;
+
+        //each pointer starts from both ends of the height array, and each of the values are evaluated to calculate the max container area
+        while(lPtr < rPtr) {
+
+            //this calculates and stores the max container area
+            //the width is found by subtracting the right and left pointer in order to find the distance
+            //the height is found by choosing the minimum height between the left and right pointer elements
+            maxArea = Math.max(maxArea, (rPtr - lPtr) * Math.min(height[lPtr], height[rPtr]));
+
+            //movcs the pointer that contains the smaller height
+            if(height[lPtr] < height[rPtr])
+                ++lPtr;
+            else
+                --rPtr;
+        }
+
+        return maxArea;
     }
 }
