6b6a22d05f5a1947a70ef84352dc1ba04f55a847
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-17 20:08:56 -0500

    These files and folders contain the information, project and boilerplate code for the 3Sum problem for v4 solution

---
+++ Top Interview 150/02. Two Pointers/05. 3Sum/v4/3Sum/src/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Arrays;
+
+public class Solution {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Set<List<Integer>> res = new HashSet<>();
+
+
+
+        return res.stream().toList();
+    }
+}
