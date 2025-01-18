620723833d026a9e667017a3e0a1108da341b80c
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-17 20:41:35 -0500

    This code implementation passes ALL test cases for the 3Sum problem with v4 solution, although, the runtime is further improved, comments also added

--- Top Interview 150/02. Two Pointers/05. 3Sum/v4/3Sum/src/Solution.java
+++ Top Interview 150/02. Two Pointers/05. 3Sum/v4/3Sum/src/Solution.java
@@ -1,5 +1,4 @@
-import java.util.Set;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 
@@ -7,28 +6,40 @@ public class Solution {
 
     public List<List<Integer>> threeSum(int[] nums) {
 
-        Set<List<Integer>> res = new HashSet<>();
+        //creates and initializes variables
+        List<List<Integer>> res = new ArrayList<>();
 
+        //this sorts the nums array, that way we can take full advantage of the 2 pointer method, which improves runtime from O(n^2) to O(n0
         Arrays.sort(nums);
 
-        System.out.println(Arrays.toString(nums));
-
+        //iterates through the nums array to find 3 sum sets
         for(int i = 0; i < nums.length; ++i) {
 
-            if(i != 0 && nums[i - 1] == nums[i])
+            //this skips duplicate values in order to improve runtime efficiency and to avoid duplicate sets
+            if(i > 0 && nums[i - 1] == nums[i])
                 continue;
 
+            //both pointers start from each end
             int lPtr = i + 1;
             int rPtr = nums.length - 1;
 
-            while(lPtr < rPtr ) {
+            //iterates through the rest of the array where each pointer moves inwards
+            while(lPtr < rPtr) {
 
-                if(nums[i] + nums[lPtr] + nums[rPtr] == 0) {
-                    List<Integer> list = Arrays.asList(nums[i], nums[lPtr], nums[rPtr]);
-                    list.sort(null);
-                    res.add(list);
+                //this also skips duplicate values in order to improve runtime efficiency and to avoid duplicate sets
+                if(lPtr > i + 1 && nums[lPtr - 1] == nums[lPtr]) {
+                    ++lPtr;
+                    continue;
+                } else if(rPtr < nums.length - 1 && nums[rPtr + 1] == nums[rPtr]) {
+                    --rPtr;
+                    continue;
                 }
 
+                //once duplicate values have been removed and all values equal to 0, this adds the set to the result
+                if(nums[i] + nums[lPtr] + nums[rPtr] == 0)
+                    res.add(Arrays.asList(nums[i], nums[lPtr], nums[rPtr]));
+
+                //this adjusts the pointers accordingly, depending on if the total is less or greater than 0
                 if(nums[i] + nums[lPtr] + nums[rPtr] < 0)
                     ++lPtr;
                 else
@@ -36,6 +47,6 @@ public class Solution {
             }
         }
 
-        return res.stream().toList();
+        return res;
     }
 }
