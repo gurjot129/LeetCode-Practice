6f466e7d973b3e946babecab6d97377e60f56fd6
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-17 20:07:10 -0500

    This code implementation passes ALL test cases for the 3Sum problem with v3 solution, although, it is quite slow

--- Top Interview 150/02. Two Pointers/05. 3Sum/v3/3Sum/src/Solution.java
+++ Top Interview 150/02. Two Pointers/05. 3Sum/v3/3Sum/src/Solution.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Arrays;
 
 public class Solution {
 
@@ -6,7 +9,29 @@ public class Solution {
 
         Set<List<Integer>> res = new HashSet<>();
 
+        Arrays.sort(nums);
 
+        System.out.println(Arrays.toString(nums));
+
+        for(int i = 0; i < nums.length; ++i) {
+
+            int lPtr = i + 1;
+            int rPtr = nums.length - 1;
+
+            while(lPtr < rPtr ) {
+
+                if(nums[i] + nums[lPtr] + nums[rPtr] == 0) {
+                    List<Integer> list = Arrays.asList(nums[i], nums[lPtr], nums[rPtr]);
+                    list.sort(null);
+                    res.add(list);
+                }
+
+                if(nums[i] + nums[lPtr] + nums[rPtr] < 0)
+                    ++lPtr;
+                else
+                    --rPtr;
+            }
+        }
 
         return res.stream().toList();
     }
