d122c1d8b1066f64b69fd0e7bb8922163d1564b0
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-17 19:54:18 -0500

    This code implementation passes 308/313 test cases for the 3Sum problem with v2 solution, however, it did not pass Test Case #309 since the time limit exceeded

--- Top Interview 150/02. Two Pointers/05. 3Sum/v2/3Sum/src/Solution.java
+++ Top Interview 150/02. Two Pointers/05. 3Sum/v2/3Sum/src/Solution.java
@@ -1,15 +1,34 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
 
     public List<List<Integer>> threeSum(int[] nums) {
 
-        List<List<Integer>> res = new ArrayList<>();
+        Set<List<Integer>> res = new HashSet<>();
 
+        for(int i = 0; i < nums.length; ++i) {
 
+            int lPtr = i + 1;
 
-        return res;
+            while(lPtr < nums.length - 1) {
+
+                int rPtr = lPtr + 1;
+
+                while(rPtr < nums.length) {
+                    if(nums[i] + nums[lPtr] + nums[rPtr] == 0) {
+                        List<Integer> list = Arrays.asList(nums[i], nums[lPtr], nums[rPtr]);
+                        list.sort(null);
+                        res.add(list);
+                    }
+
+                    ++rPtr;
+                }
+
+                ++lPtr;
+            }
+
+        }
+
+        return res.stream().toList();
     }
 }
