0b716c88f4b1d13b1bbbaf4785a534e131b5cfd0
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-21 21:09:45 -0500

    This code implementation passes ALL test cases for the Minimum Size Sub-Array Sum problem with v3 solution, it is similar to the v2 solution

--- Top Interview 150/03. Sliding Window/01. Minimum Size Subarray Sum/v3/Minimum Size Subarray Sum/src/Solution.java
+++ Top Interview 150/03. Sliding Window/01. Minimum Size Subarray Sum/v3/Minimum Size Subarray Sum/src/Solution.java
@@ -1,9 +1,26 @@
-import java.util.logging.LoggingPermission;
-
 public class Solution {
 
     public int minSubArrayLen(int target, int[] nums) {
 
-       return 0;
+        //creates and initializes variables
+        int lPtr = 0;
+        int total = 0;
+        int minSubArraySize = Integer.MAX_VALUE;
+
+        //iterates through the nums array
+        for(int rPtr = 0; rPtr < nums.length; ++rPtr) {
+
+            total += nums[rPtr];
+
+            //while the total is greater than the target, shift the left pointer forward to decrease the window
+            while(total >= target) {
+                minSubArraySize = Math.min(minSubArraySize, rPtr - lPtr + 1);
+                total -= nums[lPtr];
+                ++lPtr;
+            }
+        }
+
+        //if a subarray wasn't found, return 0, otherwise, return the minimum subarray size
+        return minSubArraySize == Integer.MAX_VALUE ? 0 : minSubArraySize;
     }
 }
