86c7c4874fb636ea2c5cd053bdbc3d20ff537334
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-22 19:48:48 -0500

    These files and folders contain the information, project and boilerplate code for the Longest Substring Without Repeating Characters problem for v1 solution

---
+++ Top Interview 150/03. Sliding Window/02. Longest Substring Without Repeating Characters/v1/Longest Substring Without Repeating Characters/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        //test case #1
+        String s = "abcabcbb";
+
+        int result = solution.lengthOfLongestSubstring(s);
+
+        System.out.println("*** FINAL RESULTS ***");
+        System.out.println("Longest Substring Without Repeating Characters: ");
+        System.out.println(result);
+    }
+}
