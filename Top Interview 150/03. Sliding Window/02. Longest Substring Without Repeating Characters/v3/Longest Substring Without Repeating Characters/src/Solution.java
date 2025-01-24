8421ecc5d26f9e581868abe4ae486f0f73e03586
Gurjot Saini <gurjot.saini129@gmail.com>
2025-01-24 02:17:17 -0500

    This code implementation passes ALL test cases for the Longest Substring without Repeating Characters problem with v3 solution

--- Top Interview 150/03. Sliding Window/02. Longest Substring Without Repeating Characters/v3/Longest Substring Without Repeating Characters/src/Solution.java
+++ Top Interview 150/03. Sliding Window/02. Longest Substring Without Repeating Characters/v3/Longest Substring Without Repeating Characters/src/Solution.java
@@ -1,7 +1,30 @@
+import java.util.HashSet;
+
 public class Solution {
 
     public int lengthOfLongestSubstring(String s) {
 
-        return 0;
+        //creates and initializes variables
+        int longestSubstring = 0;
+        HashSet<Character> uniqueCharSet = new HashSet<>(); //HashSet is used since duplicate values and order is not necessary, and to increase search/remove efficiency
+        int lPtr = 0;
+
+        //iterates through the s string character values
+        for(int rPtr = 0; rPtr < s.length(); ++rPtr) {
+
+            //as long as a duplicate exists in the HashSet, it keeps removing characters from the left, and shifts the left pointer forward
+            while(uniqueCharSet.contains(s.charAt(rPtr))) {
+                uniqueCharSet.remove(s.charAt(lPtr));
+                ++lPtr;
+            }
+
+            //adds a character to the set
+            uniqueCharSet.add(s.charAt(rPtr));
+
+            //recalculates longest substring length
+            longestSubstring = Math.max(longestSubstring, rPtr - lPtr + 1);
+        }
+
+        return longestSubstring;
     }
 }
