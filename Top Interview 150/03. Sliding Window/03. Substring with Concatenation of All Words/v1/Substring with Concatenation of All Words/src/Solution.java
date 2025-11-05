97758d74b6b7ea072f4de5e822b485683f418aa1
Gurjot Saini <gurjot.saini129@gmail.com>
2025-11-04 22:13:25 -0500

    This code implementation passes 1/3 test cases for the Longest Substring with Repeating Characters problem with v1 solution, however, it did not pass the other test cases since the implemention is not finished

--- Top Interview 150/03. Sliding Window/03. Substring with Concatenation of All Words/v1/Substring with Concatenation of All Words/src/Solution.java
+++ Top Interview 150/03. Sliding Window/03. Substring with Concatenation of All Words/v1/Substring with Concatenation of All Words/src/Solution.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ForkJoinWorkerThread;
 
 public class Solution {
 
@@ -8,7 +9,37 @@ public class Solution {
         //creates and initializes variables
         List<Integer> startingIndices = new ArrayList<Integer>();
 
-        
+        if(s != null && !s.isEmpty()) {
+
+            List<Integer> wordsFoundIdcs = new ArrayList<Integer>();
+
+            for(int i = 0; i < (s.length() - (words[0].length() * words.length)); ++i) {
+
+                int lPtr = i;
+                int rPtr = lPtr + words[0].length() - 1;
+
+                for(int j = 0; j < words.length; ++j) {
+
+                    if(!wordsFoundIdcs.contains(j)) {
+
+                        String substrTest = s.substring(lPtr, rPtr + 1);
+                        boolean equalsTest = substrTest.equals(words[j]);
+
+                        if(s.substring(lPtr, rPtr + 1).equals(words[j])) {
+                            wordsFoundIdcs.add(j);
+                            lPtr += words[0].length();
+                            rPtr += words[0].length();
+                        }
+                    }
+                }
+
+
+
+                if (wordsFoundIdcs.size() == words.length) {
+                    startingIndices.add(i);
+                }
+            }
+        }
 
         return startingIndices;
     }
