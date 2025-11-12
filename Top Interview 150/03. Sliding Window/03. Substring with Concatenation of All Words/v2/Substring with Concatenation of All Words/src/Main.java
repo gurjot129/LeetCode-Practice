d32deeba7c4ee42fd6d393a09cb5f05b49993fc7
Gurjot Saini <gurjot.saini129@gmail.com>
2025-11-11 21:38:38 -0500

    These files and folders contain the information, project and boilerplate code for the Substring with Concatenation of All Words problem for v2 solution

---
+++ Top Interview 150/03. Sliding Window/03. Substring with Concatenation of All Words/v2/Substring with Concatenation of All Words/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        //test case #1
+//        String s = "barfoothefoobarman";
+//        String[] words = {"foo","bar"};
+
+        //test case #134
+        String s = "wordgoodgoodgoodbestword";
+        String[] words = {"word","good","best","good"};
+
+        List<Integer> result = solution.findSubstring(s, words);
+
+        System.out.println("*** FINAL RESULTS ***");
+        System.out.println("Starting indices of all the concatenated substrings in s: ");
+        System.out.println(result);
+    }
+}
