5f05746e89c046b69534d1b548acaa5dd4811268
Gurjot Saini <gurjot.saini129@gmail.com>
2025-11-16 19:20:23 -0500

    These files and folders contain the information, project and boilerplate code for the Minimum Window Substring problem for v2 solution

---
+++ Top Interview 150/03. Sliding Window/04. Minimum Window Substring/v2/Minimum Window Substring/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        //test case #1
+//        String s = "ADOBECODEBANC";
+//        String t = "ABC";
+
+        //test case #193
+        String s = "aaaaaaaaaaaabbbbbcdd";
+        String t = "abcdd";
+
+        String result = solution.minWindow(s, t);
+
+        System.out.println("*** FINAL RESULTS ***");
+        System.out.println("Minimum window substring in s: ");
+        System.out.println(result);
+    }
+}
---
+++ Top Interview 150/03. Sliding Window/04. Minimum Window Substring/v2/Minimum Window Substring/src/Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+
+    public String minWindow(String s, String t) {
+
+        return "";
+    }
+}
