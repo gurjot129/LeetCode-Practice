87a064986b7600e40e28bfad8a833ecbf66c12e4
Gurjot Saini <gurjot.saini129@gmail.com>
2025-11-17 21:51:32 -0500

    These files and folders contain the information, project and boilerplate code for the Valid Sudoku problem for v1 solution

---
+++ Top Interview 150/04. Matrix/01. Valid Sudoku/v1/Valid Sudoku/src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        //test case #1
+        char[][] board = {{'5','3','.','.','7','.','.','.','.'},
+                          {'6','.','.','1','9','5','.','.','.'},
+                          {'.','9','8','.','.','.','.','6','.'},
+                          {'8','.','.','.','6','.','.','.','3'},
+                          {'4','.','.','8','.','3','.','.','1'},
+                          {'7','.','.','.','2','.','.','.','6'},
+                          {'.','6','.','.','.','.','2','8','.'},
+                          {'.','.','.','4','1','9','.','.','5'},
+                          {'.','.','.','.','8','.','.','7','9'}};
+
+        boolean result = solution.isValidSudoku(board);
+
+        System.out.println("*** FINAL RESULTS ***");
+        System.out.println("Is Valid Sudoku: ");
+        System.out.println(result);
+        
+    }
+}
---
+++ Top Interview 150/04. Matrix/01. Valid Sudoku/v1/Valid Sudoku/src/Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+
+    public boolean isValidSudoku(char[][] board) {
+
+        return true;
+    }
+}
