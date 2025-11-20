29c257324b9dcb00c1e6417d291401430c5c4170
Gurjot Saini <gurjot.saini129@gmail.com>
2025-11-19 22:27:18 -0500

    These files and folders contain the information, project and boilerplate code for the Spiral Matrix problem for v1 solution

---
+++ Top Interview 150/04. Matrix/02. Spiral Matrix/v1/Spiral Matrix/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        //test case #1
+        int[][] matrix = {
+                            {1, 2, 3},
+                            {4, 5, 6},
+                            {7, 8, 9}
+                        };
+
+        List<Integer> result = solution.spiralOrder(matrix);
+
+        System.out.println("*** FINAL RESULTS ***");
+        System.out.println("Spiral Matrix Solution: ");
+        System.out.println(result);
+
+    }
+}
---
+++ Top Interview 150/04. Matrix/02. Spiral Matrix/v1/Spiral Matrix/src/Solution.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        return new ArrayList<Integer>();
+    }
+}
