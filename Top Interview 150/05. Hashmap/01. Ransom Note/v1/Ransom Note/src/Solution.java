c19fad1a2b7f938afb9fe1d143e1b01fec90b6c4
Gurjot Saini <gurjot.saini129@gmail.com>
2026-01-06 22:10:45 -0500

    Adds additional tweaks and comments to Ransom Note solution v1

--- Top Interview 150/05. Hashmap/01. Ransom Note/v1/Ransom Note/src/Solution.java
+++ Top Interview 150/05. Hashmap/01. Ransom Note/v1/Ransom Note/src/Solution.java
@@ -4,20 +4,24 @@ public class Solution {
 
     public boolean canConstruct(String ransomNote, String magazine) {
 
+        //creates and initializes variables
         HashMap<Character, Integer> rnCount = new HashMap<>();
 
+        //creates hashmap for ransom note
         for(char c : ransomNote.toCharArray()) {
-            if(rnCount.get(c) == null) {
-                rnCount.put(c, 1);
-            } else {
+            if(rnCount.containsKey(c)) {
                 rnCount.put(c, rnCount.get(c) + 1);
+            } else {
+                rnCount.put(c, 1);
             }
         }
 
+        //compares ransom note characters to the magazine array
         for(int i = 0; i < magazine.length() && !rnCount.isEmpty(); ++i) {
 
             char c = magazine.charAt(i);
 
+            //if ransom note character is found in magazine, decrement value in ransom note hashmap and remove it if 0
             if(rnCount.get(c) != null) {
                 rnCount.put(c, rnCount.get(c) - 1);
 
@@ -26,6 +30,7 @@ public class Solution {
             }
         }
 
+        //if all the ransom note characters were found in the magazine, the hashmap should be empty and returning true
         return rnCount.isEmpty();
     }
 }
