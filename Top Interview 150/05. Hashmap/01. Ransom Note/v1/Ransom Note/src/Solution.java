ebc522729a776dde33ff7c3d21b5b598345f7dfa
Gurjot Saini <gurjot.saini129@gmail.com>
2026-01-06 21:52:23 -0500

    This code implementation passes ALL test case for the Ransom Note problem with v1 solution

--- Top Interview 150/05. Hashmap/01. Ransom Note/v1/Ransom Note/src/Solution.java
+++ Top Interview 150/05. Hashmap/01. Ransom Note/v1/Ransom Note/src/Solution.java
@@ -1,7 +1,31 @@
+import java.util.HashMap;
+
 public class Solution {
 
     public boolean canConstruct(String ransomNote, String magazine) {
 
-        return true;
+        HashMap<Character, Integer> rnCount = new HashMap<>();
+
+        for(char c : ransomNote.toCharArray()) {
+            if(rnCount.get(c) == null) {
+                rnCount.put(c, 1);
+            } else {
+                rnCount.put(c, rnCount.get(c) + 1);
+            }
+        }
+
+        for(int i = 0; i < magazine.length() && !rnCount.isEmpty(); ++i) {
+
+            char c = magazine.charAt(i);
+
+            if(rnCount.get(c) != null) {
+                rnCount.put(c, rnCount.get(c) - 1);
+
+                if(rnCount.get(c) == 0)
+                    rnCount.remove(c);
+            }
+        }
+
+        return rnCount.isEmpty();
     }
 }
