fe07a7648dfc387e8c195bddecf7f9a96240ed7f
Gurjot Saini <gurjot.saini129@gmail.com>
2026-01-10 22:29:54 -0500

    This code implementation passes 40/47 test cases for the Isomorphic Strings problem with v1 solution, however, test case 41 failed since it wasn't meeting the isomorphic condition, since the code was strictly following a key-value pair approach and not vice-versa

--- Top Interview 150/05. Hashmap/01. Ransom Note/v1/Isomorphic Strings/src/Solution.java
+++ Top Interview 150/05. Hashmap/01. Ransom Note/v1/Isomorphic Strings/src/Solution.java
@@ -1,8 +1,19 @@
+import java.util.HashMap;
+
 public class Solution {
 
     public boolean isIsomorphic(String s, String t) {
 
-        
+        HashMap<Character, Character> charOccurrences = new HashMap<>();
+
+        for(int i = 0; i < s.length(); ++i) {
+
+            if(charOccurrences.containsKey(s.charAt(i)) && charOccurrences.get(s.charAt(i)) != t.charAt(i)) {
+                return false;
+            } else if(charOccurrences.containsKey(s.charAt(i)) == false) {
+                charOccurrences.put(s.charAt(i), t.charAt(i));
+            }
+        }
 
         return true;
     }
