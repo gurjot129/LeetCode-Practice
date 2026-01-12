01dbfcd888bc01a0fb015cbe2751ec93b201e46c
Gurjot Saini <gurjot.saini129@gmail.com>
2026-01-11 19:58:55 -0500

    These files and folders contain the information, project and boilerplate code for the Word Pattern problem for v1 solution

---
+++ Top Interview 150/05. Hashmap/03. Word Pattern/v1/Word Pattern/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        //test case #1
+        String pattern = "abba";
+        String s = "dog cat cat dog";
+
+        boolean result = solution.wordPattern(pattern, s);
+
+        System.out.println("*** FINAL RESULTS ***");
+        System.out.println("Solution: ");
+        System.out.println(result);
+    }
+}
---
+++ Top Interview 150/05. Hashmap/03. Word Pattern/v1/Word Pattern/src/Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+
+    public boolean wordPattern(String pattern, String s) {
+
+        return true;
+    }
+}
