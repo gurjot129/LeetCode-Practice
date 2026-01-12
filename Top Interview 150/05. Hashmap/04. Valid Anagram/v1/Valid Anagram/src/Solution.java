395dccbd075102d40d557d2d20d619ba9eb915e6
Gurjot Saini <gurjot.saini129@gmail.com>
2026-01-11 22:41:46 -0500

    This code implementation passes 49/54 test cases for the Valid Anagram problem with v1 solution, however, test case #50 failed due to having similar characters between the strings while not having the same number of characters matching

--- Top Interview 150/05. Hashmap/04. Valid Anagram/v1/Valid Anagram/src/Solution.java
+++ Top Interview 150/05. Hashmap/04. Valid Anagram/v1/Valid Anagram/src/Solution.java
@@ -1,7 +1,28 @@
+import java.util.TreeMap;
+
 public class Solution {
 
     public boolean isAnagram(String s, String t) {
-        
-        return true;
+
+        //ensures that both strings are the same length
+        if(s.length() != t.length())
+            return false;
+
+        //creates and initializes variables
+        TreeMap<Character, Integer> sMap = new TreeMap<>();
+        TreeMap<Character, Integer> tMap = new TreeMap<>();
+
+        //splits string character into char array
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        //appends char values into tree map
+        for(int i = 0; i < s.length(); ++i) {
+            sMap.put(sArray[i], 0);
+            tMap.put(tArray[i], 0);
+        }
+
+        //returns true or false depending on if the strings are equals or not
+        return sMap.equals(tMap);
     }
 }
