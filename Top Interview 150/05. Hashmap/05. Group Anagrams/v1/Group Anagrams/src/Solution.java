7aab0e71274420cca3eacf985de5c4819a276f50
Gurjot Saini <gurjot.saini129@gmail.com>
2026-01-12 22:49:50 -0500

    This code implementation passes ALL test cases for the Group Anagrams problem with v1 solution

--- Top Interview 150/05. Hashmap/05. Group Anagrams/v1/Group Anagrams/src/Solution.java
+++ Top Interview 150/05. Hashmap/05. Group Anagrams/v1/Group Anagrams/src/Solution.java
@@ -1,12 +1,43 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Solution {
 
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        
+        //creates and initializes variables
+        List<List<String>> ga = new ArrayList<>();
+        HashMap<String, Integer> gaIdx = new HashMap<>();
+        int idx = 0;
 
-        return new ArrayList<List<String>>();
+        //iterates through strs array and evaluates each value
+        for(String s : strs) {
+
+            //sorts each string, in order for it to represent a key, that will hold the index value of the array list
+            String sortedS = s;
+            char[] sCharArr = sortedS.toCharArray();
+            Arrays.sort(sCharArr);
+            sortedS = new String(sCharArr);
+
+            //if anagram not found, then add a new arraylist to the list
+            boolean isAnagramFound = false;
+
+            //if the sorted array (which represents a key) exists in the hashmap, it exists in the arraylist, therefore, add the string to the respective arraylist (with the appropriate arraylist index)
+            if(gaIdx.containsKey(sortedS)) {
+                ga.get(gaIdx.get(sortedS)).add(s);
+                isAnagramFound = true;
+            }
+
+            //if arraylist is empty OR anagram not found, then add a new arraylist to the list
+            if(ga.isEmpty() || !isAnagramFound) {
+                gaIdx.put(sortedS, idx++);
+                ga.add(new ArrayList<String>(List.of(s)));
+            }
+
+        }
+
+        return ga;
     }
 }
