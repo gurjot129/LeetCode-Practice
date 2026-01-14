253e6dd20c758aa6685dcc43bd30bb8e3d10cdf5
Gurjot Saini <gurjot.saini129@gmail.com>
2026-01-13 23:19:14 -0500

    This code implementation passes ALL test cases for the Two Sum problem with v1 solution

--- Top Interview 150/05. Hashmap/06. Two Sum/v1/Two Sum/src/Main.java
+++ Top Interview 150/05. Hashmap/06. Two Sum/v1/Two Sum/src/Main.java
@@ -5,13 +5,21 @@ public class Main {
         Solution solution = new Solution();
 
         //test case #1
-        int[] nums = {2,7,11,15};
-        int target = 9;
+//        int[] nums = {2,7,11,15};
+//        int target = 9;
+
+        //test case #3
+        int[] nums = {3, 2, 4};
+        int target = 6;
+
+        //test case #3
+//        int[] nums = {3, 3};
+//        int target = 6;
 
         int[] result = solution.twoSum(nums, target);
 
         System.out.println("*** FINAL RESULTS ***");
         System.out.println("Solution: ");
-        System.out.println(result);
+        System.out.println("[ " + result[0] + ", " + result[1] + " ]");
     }
 }
--- Top Interview 150/05. Hashmap/06. Two Sum/v1/Two Sum/src/Solution.java
+++ Top Interview 150/05. Hashmap/06. Two Sum/v1/Two Sum/src/Solution.java
@@ -1,6 +1,26 @@
+import java.util.HashMap;
+
 public class Solution {
 
     public int[] twoSum(int[] nums, int target) {
 
+        //creates and initializes variables
+        HashMap<Integer, Integer> numIdcs = new HashMap<>();
+
+        //stores the values with the associated index
+        for(int i = 0; i < nums.length; ++i)
+            numIdcs.put(nums[i], i);
+
+        //iterates and evaluates all the values in the nums array
+        for(int i = 0; i < nums.length; ++i) {
+
+            //when subtracting the current element from target, if the result is found in the hashmap, return the index of both the current value and the result value
+            if(numIdcs.containsKey(target - nums[i]) && numIdcs.get(target - nums[i]) != i) {
+                return new int[]{numIdcs.get(target - nums[i]), i};
+            }
+        }
+
+
+        return new int[]{};
     }
 }
