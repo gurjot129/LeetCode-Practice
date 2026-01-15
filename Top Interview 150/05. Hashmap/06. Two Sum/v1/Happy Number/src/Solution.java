537db6353bacfe212261c76780073570c6e717ae
Gurjot Saini <gurjot.saini129@gmail.com>
2026-01-14 22:46:06 -0500

    This code implementation passes the 1st test case for the Happy Number problem with v1 solution, however, it doesn't handle the case where it is not a happy number, causing a stack overflow

--- Top Interview 150/05. Hashmap/06. Two Sum/v1/Happy Number/src/Solution.java
+++ Top Interview 150/05. Hashmap/06. Two Sum/v1/Happy Number/src/Solution.java
@@ -2,6 +2,23 @@ public class Solution {
 
     public boolean isHappy(int n) {
 
+        //while n is not equal to 1, continue to perform calculations until conditon is met
+        while(n != 1) {
+
+            //seperate each digit
+            char[] nums = ("" + n).toCharArray();
+
+            //reset total to 0
+            n = 0;
+
+            //perform exponent calculations
+            for(char c : nums) {
+                n += (int) Math.pow((c - '0'), 2);
+            }
+
+            System.out.println("Total: " + n);
+        }
+
         return true;
     }
 }
