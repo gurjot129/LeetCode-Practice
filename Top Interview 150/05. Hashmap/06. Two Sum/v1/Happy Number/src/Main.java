54818ce8faecb659fbd801d452448454a96558d7
Gurjot Saini <gurjot.saini129@gmail.com>
2026-01-14 22:54:45 -0500

    This code implementation passes ALL test cases for the Happy Number problem with v1 solution

--- Top Interview 150/05. Hashmap/06. Two Sum/v1/Happy Number/src/Main.java
+++ Top Interview 150/05. Hashmap/06. Two Sum/v1/Happy Number/src/Main.java
@@ -4,7 +4,13 @@ public class Main {
         Solution solution = new Solution();
 
         //test case #1
-        int n = 19;
+//        int n = 19;
+
+        //test case #1
+//        int n = 2;
+
+        //test case #419
+        int n = 1111111;
 
         boolean result = solution.isHappy(n);
 
--- Top Interview 150/05. Hashmap/06. Two Sum/v1/Happy Number/src/Solution.java
+++ Top Interview 150/05. Hashmap/06. Two Sum/v1/Happy Number/src/Solution.java
@@ -3,7 +3,7 @@ public class Solution {
     public boolean isHappy(int n) {
 
         //while n is not equal to 1, continue to perform calculations until conditon is met
-        while(n != 1) {
+        while(n != 1 && n != 7) {
 
             //seperate each digit
             char[] nums = ("" + n).toCharArray();
@@ -17,6 +17,11 @@ public class Solution {
             }
 
             System.out.println("Total: " + n);
+
+            //return false if the total is a single digit that is NOT a 1 or 7
+            if(("" + n).length() == 1 && n != 1 && n != 7) {
+                return false;
+            }
         }
 
         return true;
