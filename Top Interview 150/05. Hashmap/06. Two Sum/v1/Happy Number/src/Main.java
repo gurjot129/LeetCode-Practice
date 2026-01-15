fce5614b82ecb592fc592c96799b0a6c242d9d3c
Gurjot Saini <gurjot.saini129@gmail.com>
2026-01-14 22:26:43 -0500

    These files and folders contain the information, project and boilerplate code for the Happy Number problem for v1 solution

---
+++ Top Interview 150/05. Hashmap/06. Two Sum/v1/Happy Number/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        //test case #1
+        int n = 19;
+
+        boolean result = solution.isHappy(n);
+
+        System.out.println("*** FINAL RESULTS ***");
+        System.out.println("Solution: ");
+        System.out.println(result);
+    }
+}
