618d5c0c612c14979881f37e55d6c3808847c801
Gurjot Saini <gurjot.saini129@gmail.com>
2026-03-24 00:19:43 -0400

    This code implementation passes ALL test cases for the Contains Duplicate II problem with v1 solution

--- Top Interview 150/05. Hashmap/08. Contains Duplicate II/v1/Contains Duplicate II/src/Solution.java
+++ Top Interview 150/05. Hashmap/08. Contains Duplicate II/v1/Contains Duplicate II/src/Solution.java
@@ -1,7 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
     public boolean containsNearbyDuplicate(int[] nums, int k) {
 
-        return true;
+        //creates and initializes variables
+        HashMap<Integer, ArrayList<Integer>> dupIndices = new HashMap<>();
+
+        //stores indices of same integer values
+        for(int i = 0; i < nums.length; ++i) {
+
+            if (!dupIndices.containsKey(nums[i])) {
+                dupIndices.put(nums[i], new ArrayList<Integer>());
+            }
+
+            dupIndices.get(nums[i]).add(i);
+        }
+
+        //loops through the hash map and checks for the condition abs(i - j) <= k
+        for (Map.Entry<Integer, ArrayList<Integer>> entry : dupIndices.entrySet()) {
+
+            ArrayList<Integer> value = entry.getValue();
+
+            //ensures that there are at least 2 values in the array
+            if(value.size() > 1) {
+
+                //checks for the condition abs(i - j) <= k
+                for(int i = 1; i < value.size(); ++i) {
+                    if (Math.abs(value.get(i) - value.get(i - 1)) <= k)
+                        return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
