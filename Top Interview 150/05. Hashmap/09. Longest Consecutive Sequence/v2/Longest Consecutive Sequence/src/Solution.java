8969ba2b0ecc8479640b9d19e08f640ff96e2f4f
Gurjot Saini <gurjot.saini129@gmail.com>
2026-03-31 21:55:34 -0400

    This code implementation passes ALL test cases for the Longest Consecutive Sequence problem with v2 solution

--- Top Interview 150/05. Hashmap/09. Longest Consecutive Sequence/v2/Longest Consecutive Sequence/src/Solution.java
+++ Top Interview 150/05. Hashmap/09. Longest Consecutive Sequence/v2/Longest Consecutive Sequence/src/Solution.java
@@ -1,7 +1,38 @@
+import java.util.HashSet;
+
 public class Solution {
 
     public int longestConsecutive(int[] nums) {
 
-        return 0;
+        //creates and initializes variables
+        HashSet<Integer> numsSet = new HashSet<Integer>();
+        int res = 0;
+
+        //copy nums elements to set in order to remove duplicates and to quickly find values
+        for(int i : nums)
+            numsSet.add(i);
+
+        //find longest consecutive sequence
+        for(int i : numsSet) {
+
+            //stores potential start of the consecutive number
+            int currNum = i;
+
+            //if there is no value before the current number, let this be the starting number
+            if(!numsSet.contains(currNum - 1)) {
+
+                int currConsecSeqLen = 1;
+                res = Math.max(currConsecSeqLen, res);
+
+                //increments result length when next consecutive value is found
+                while(numsSet.contains(currNum + 1)) {
+                    res = Math.max(++currConsecSeqLen, res);
+                    ++currNum;
+                }
+            }
+
+        }
+
+        return res;
     }
 }
