2c050a3d68011a30444e17081eb52e996e968ff4
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-06 16:01:47 -0400

    These files and folders contain the information, project and boilerplate code for the Valid Parentheses problem for v1 solution

---
+++ Top Interview 150/07. Stack/01. Valid Parentheses/v1/Valid Parentheses/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        //test case #1
+        String s = "()";
+
+        boolean result = solution.isValid(s);
+
+        System.out.println("*** FINAL RESULTS ***");
+        System.out.println("Solution: " + result);
+    }
+}
