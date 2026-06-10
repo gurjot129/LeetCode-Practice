454cefb45e3c178e1f8c87cd4323faa837305fa1
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-10 00:04:07 -0400

    This code implementation passes ALL test cases for the Min Stack problem with v2 solution

--- Top Interview 150/07. Stack/03. Min Stack/v2/Min Stack/src/MinStack.java
+++ Top Interview 150/07. Stack/03. Min Stack/v2/Min Stack/src/MinStack.java
@@ -1,23 +1,48 @@
+import java.util.Stack;
+
 class MinStack {
 
-    public MinStack() {
+    //creates instance variables
+    private Stack<Integer> stack; //keeps track of all values
+    private Stack<Integer> minStack; //keeps track of minimum values
 
+    //constructor
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
     }
 
-    public void push(int value) {
+    public void push(int val) {
 
+        stack.push(val);
+
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+
+            //only adds minimum values
+            if (val <= minStack.peek()) {
+                minStack.push(val);
+            }
+        }
     }
 
     public void pop() {
 
+        //checks to see if the top value matches the min stack top value, in order to pop
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+
+        stack.pop();
     }
 
     public int top() {
-
+        return stack.peek();
     }
 
     public int getMin() {
-
+        return minStack.peek();
     }
 }
 
