d2a39eb87b89c89ca7d42a323e1067d45ca8803a
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-10 01:12:40 -0400

    This code implementation passes ALL test cases for the Evaluate RPN problem with v1 solution

--- Top Interview 150/07. Stack/04. Evaluate Reverse Polish Notation/v1/Evaluate Reverse Polish Notation/src/Main.java
+++ Top Interview 150/07. Stack/04. Evaluate Reverse Polish Notation/v1/Evaluate Reverse Polish Notation/src/Main.java
@@ -5,7 +5,10 @@ public class Main {
         Solution solution = new Solution();
 
         //test case #1
-        String[] s = {"2","1","+","3","*"};
+//        String[] s = {"2","1","+","3","*"};
+
+        //test case #2
+        String[] s = {"4","13","5","/","+"};
 
         int result = solution.evalRPN(s);
 
--- Top Interview 150/07. Stack/04. Evaluate Reverse Polish Notation/v1/Evaluate Reverse Polish Notation/src/Solution.java
+++ Top Interview 150/07. Stack/04. Evaluate Reverse Polish Notation/v1/Evaluate Reverse Polish Notation/src/Solution.java
@@ -4,34 +4,39 @@ class Solution {
 
     public int evalRPN(String[] tokens) {
 
+        //creates and initializes variables
         Stack<String> stack = new Stack<>();
 
+        //iterates through and evaluates each token
         for(String t : tokens) {
 
+            //helps store parsed values for evaluation
             int num1 = 0;
             int num2 = 0;
 
+            //if it's a number, push into stack
+            //if arithmetic operator, perform relevant arithmetic operation
             switch (t) {
 
                 case "+":
                     num1 = Integer.parseInt(stack.pop());
                     num2 = Integer.parseInt(stack.pop());
-                    stack.push((num1 + num2) + "");
+                    stack.push((num2 + num1) + "");
                     break;
                 case "-":
                     num1 = Integer.parseInt(stack.pop());
                     num2 = Integer.parseInt(stack.pop());
-                    stack.push((num1 - num2) + "");
+                    stack.push((num2 - num1) + "");
                     break;
                 case "*":
                     num1 = Integer.parseInt(stack.pop());
                     num2 = Integer.parseInt(stack.pop());
-                    stack.push((num1 * num2) + "");
+                    stack.push((num2 * num1) + "");
                     break;
                 case "/":
                     num1 = Integer.parseInt(stack.pop());
                     num2 = Integer.parseInt(stack.pop());
-                    stack.push((num1 / num2) + "");
+                    stack.push((num2 / num1) + "");
                     break;
                 default:
                     stack.push(t);
