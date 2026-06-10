6dde6b05c67c3e7de23459cd60b4a61b860efba3
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-10 01:02:29 -0400

    This code implementation passes the 1st test case for the Evaluate RPN problem with v1 solution, however, the 2nd and 3rd test case fails due to incorrect output

--- Top Interview 150/07. Stack/04. Evaluate Reverse Polish Notation/v1/Evaluate Reverse Polish Notation/src/Solution.java
+++ Top Interview 150/07. Stack/04. Evaluate Reverse Polish Notation/v1/Evaluate Reverse Polish Notation/src/Solution.java
@@ -1,7 +1,44 @@
+import java.util.Stack;
+
 class Solution {
 
     public int evalRPN(String[] tokens) {
 
-        return 0;
+        Stack<String> stack = new Stack<>();
+
+        for(String t : tokens) {
+
+            int num1 = 0;
+            int num2 = 0;
+
+            switch (t) {
+
+                case "+":
+                    num1 = Integer.parseInt(stack.pop());
+                    num2 = Integer.parseInt(stack.pop());
+                    stack.push((num1 + num2) + "");
+                    break;
+                case "-":
+                    num1 = Integer.parseInt(stack.pop());
+                    num2 = Integer.parseInt(stack.pop());
+                    stack.push((num1 - num2) + "");
+                    break;
+                case "*":
+                    num1 = Integer.parseInt(stack.pop());
+                    num2 = Integer.parseInt(stack.pop());
+                    stack.push((num1 * num2) + "");
+                    break;
+                case "/":
+                    num1 = Integer.parseInt(stack.pop());
+                    num2 = Integer.parseInt(stack.pop());
+                    stack.push((num1 / num2) + "");
+                    break;
+                default:
+                    stack.push(t);
+
+            }
+        }
+
+        return Integer.parseInt(stack.peek());
     }
 }
