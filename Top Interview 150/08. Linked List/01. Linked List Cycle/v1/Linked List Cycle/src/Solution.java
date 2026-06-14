152a5c257457fce3e2962c6235bf9476f24ce80e
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-14 01:45:13 -0400

    This code implementation passes 28/29 test cases for the Linked List Cycle problem with v1 solution, however, it fails test case #29 since time limit is exceeded, potentially due to use of the Hashmap

--- Top Interview 150/08. Linked List/01. Linked List Cycle/v1/Linked List Cycle/src/Solution.java
+++ Top Interview 150/08. Linked List/01. Linked List Cycle/v1/Linked List Cycle/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * Definition for singly-linked list.
  * class ListNode {
@@ -12,5 +14,27 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
 
+        //creates and initializes variables
+        ListNode curr = head;
+        HashMap<Integer, String> nodeRefs = new HashMap<>();
+        boolean isCycle = false;
+        int currPos = 0;
+
+        //iterates through the linked list until a repeated node is found, or null is reached
+        while(curr != null && !isCycle) {
+//            System.out.println(curr);
+//            System.out.println(curr.val);
+//            System.out.println("");
+
+            //stores and checks in hashmap for any repeated node references
+            if(!nodeRefs.containsValue(curr.toString())) {
+                nodeRefs.put(currPos++, curr.toString());
+                curr = curr.next;
+            } else {
+                isCycle = true;
+            }
+        }
+
+        return isCycle;
     }
 }
