c157b3103452115c7a483855f182dff8f93b65e5
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-16 22:38:54 -0400

    This code implementation passes ALL test cases for the Add Two Numbers problem with v2 solution

--- Top Interview 150/08. Linked List/02. Add Two Numbers/v2/Add Two Numbers/src/Solution.java
+++ Top Interview 150/08. Linked List/02. Add Two Numbers/v2/Add Two Numbers/src/Solution.java
@@ -11,6 +11,26 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        return null;
+        //creates and initializes variables
+        ListNode result = new ListNode(0);
+        ListNode resCurr = result;
+        int quotient = 0;
+
+        while(l1 != null || l2 != null || quotient != 0) {
+
+            //calculates current nodes value total
+            int currNodesTotal = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + quotient;
+
+            //calculates quotient for carrying over and stores the remainder in the current result node
+            quotient = currNodesTotal / 10;
+            resCurr.next = new ListNode(currNodesTotal % 10);
+
+            //assign the next nodes to assess and store values
+            resCurr = resCurr.next;
+            l1 = l1 != null ? (l1.next != null ? l1.next : null) : null;
+            l2 = l2 != null ? (l2.next != null ? l2.next : null) : null;
+        }
+
+        return result.next != null ? result.next : result;
     }
 }
