15b83ffcd319a848d865f4a19970b532c9697f6c
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-18 00:01:33 -0400

    These files and folders contain the information, project and boilerplate code for the Copy List with Random Pointer problem for v1 solution

---
+++ Top Interview 150/08. Linked List/04. Copy List with Random Pointer/v1/Copy List with Random Pointer/src/Main.java
@@ -0,0 +1,23 @@
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        //difficult/time-consuming to implement test case
+    }
+}
