e27c9e961a31f349b368e84ae29acabe4206074e
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-24 00:08:03 -0400

    This code implementation passes ALL test cases for the Reverse Linked List II problem with v1 solution

--- Top Interview 150/08. Linked List/05. Reverse Linked List II/v1/Reverse Linked List II/src/Solution.java
+++ Top Interview 150/08. Linked List/05. Reverse Linked List II/v1/Reverse Linked List II/src/Solution.java
@@ -14,45 +14,32 @@ class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
 
         //creates and initializes variables
-        ArrayList<ListNode> leftToRightNodes = new ArrayList<>(); //stores the relevant nodes that need to be in reverse order
-        ListNode headCurr = head;
-        ListNode startNode = null; //the node before the left node, will point to the beginning of the reverse nodes
-        ListNode endNode = null; //the node before the right node, reverse nodes will point to this node
-        boolean isLeftNodeFound = false;
-        boolean isRightNodeFound = false;
+        ListNode dummy = new ListNode(0, head);
+        ListNode leftPrev = dummy;
+        ListNode curr = head;
 
-        //iterates, evaluates and stores the nodes the need to be reversed
-        while(headCurr != null && !isRightNodeFound) {
-
-            //determines starting node once left node is found
-            if(headCurr.val == left && !isLeftNodeFound) {
-                isLeftNodeFound = true;
-            } else if (!isLeftNodeFound) {
-                startNode = headCurr;
-            }
-
-            //once left node is found, the relevant nodes from left to right will be stored
-            //Also determines the end node once the right node is found
-            if(isLeftNodeFound && headCurr.val != right) {
-                leftToRightNodes.add(headCurr);
-            } else if(isLeftNodeFound) {
-                leftToRightNodes.add(headCurr);
-                isRightNodeFound = true;
-                endNode = headCurr.next;
-            }
-
-            headCurr = headCurr.next;
+        //reaches the left position node
+        for(int i = 0; i < (left - 1); ++i) {
+            leftPrev = curr;
+            curr = curr.next;
         }
 
-        //joins the start node and reverse nodes
-        for(int i = leftToRightNodes.size() - 1; i >= 0; --i) {
-            startNode.next = leftToRightNodes.get(i);
-            startNode = startNode.next;
+        //reverses the nodes from left to right positions
+        ListNode prev = null;
+
+        for(int i = 0; i < (right - left + 1); ++i) {
+
+            ListNode tempNext = curr.next;
+            curr.next = prev;
+
+            prev = curr;
+            curr = tempNext;
         }
 
-        //joins the end node
-        startNode.next = endNode;
+        //update pointers
+        leftPrev.next.next = curr;
+        leftPrev.next = prev;
 
-        return head;
+        return dummy.next;
     }
 }
