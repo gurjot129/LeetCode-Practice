72e6406a2dbf4e81e29ea77a73860d2254586248
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-27 01:21:42 -0400

    This code implementation passes ALL test cases for the Remove Nth Node From End of List problem with v2 solution

--- Top Interview 150/08. Linked List/07. Remove Nth Node From End of List/v2/Remove Nth Node From End of List/src/Solution.java
+++ Top Interview 150/08. Linked List/07. Remove Nth Node From End of List/v2/Remove Nth Node From End of List/src/Solution.java
@@ -11,6 +11,27 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
-        return null;
+        //creates and initializes variables
+        ListNode dummy = new ListNode(0, head);
+        ListNode leftPtr = dummy;
+        ListNode rightPtr = head;
+
+        //shifts the right pointer by n
+        while(n > 0 && rightPtr != null) {
+            rightPtr = rightPtr.next;
+            --n;
+        }
+
+        //once the right pointer reaches the end of the ll,
+        //the left pointer will be n distance away from the end of the ll
+        while(rightPtr != null) {
+            leftPtr = leftPtr.next;
+            rightPtr = rightPtr.next;
+        }
+
+        //removes relevant node
+        leftPtr.next = leftPtr.next.next;
+
+        return dummy.next;
     }
 }
