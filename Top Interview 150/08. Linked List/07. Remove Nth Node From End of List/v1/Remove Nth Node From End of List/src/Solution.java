c82ac9bf6f3ad53aaebd72fc7b8de2fd44e36d5f
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-27 00:03:06 -0400

    This code implementation passes 3/203 test cases for the Remove Nth Node From End of List problem with v1 solution, the 4th test case fails due to a NullPointerException Error

--- Top Interview 150/08. Linked List/07. Remove Nth Node From End of List/v1/Remove Nth Node From End of List/src/Solution.java
+++ Top Interview 150/08. Linked List/07. Remove Nth Node From End of List/v1/Remove Nth Node From End of List/src/Solution.java
@@ -11,6 +11,41 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
-        return null;
+        if(head == null) {
+            return head;
+        } else if(head.next == null) {
+            return head.next;
+        }
+
+        ListNode dummy = new ListNode(0, head);
+        ListNode curr = head;
+        int size = 0;
+        int removeNodePos = 0;
+
+        //finds total size
+        while(curr != null) {
+            ++size;
+            curr = curr.next;
+        }
+
+        removeNodePos = size - n + 1;
+
+        System.out.println("Size: " + size);
+        System.out.println("RemoveNodePos: " + removeNodePos);
+
+        curr = head;
+        ListNode prev = null;
+        ListNode next = curr.next;
+
+        for(int i = 1; i < removeNodePos; ++i) {
+            prev = curr;
+            curr = curr.next;
+            next = curr.next;
+        }
+
+        prev.next = next;
+        curr.next = null;
+
+        return dummy.next;
     }
 }
