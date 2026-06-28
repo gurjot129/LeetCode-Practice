6191bccfb978275814feeac8532a2e91e5071a16
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-27 21:08:51 -0400

    This code implementation passes ALL test cases for the Remove Duplicates from Sorted List II problem with v2 solution

--- Top Interview 150/08. Linked List/08. Remove Duplicates from Sorted List II/v2/Remove Duplicates from Sorted List II/src/Solution.java
+++ Top Interview 150/08. Linked List/08. Remove Duplicates from Sorted List II/v2/Remove Duplicates from Sorted List II/src/Solution.java
@@ -12,7 +12,34 @@ import java.util.List;
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
 
-        return null;
+        //creates and initializes variables
+        ListNode dummy = new ListNode(-101, head);
+        ListNode prev = dummy;
+        ListNode curr = head;
+
+        //iterates through the ll
+        while (curr != null && curr.next != null) {
+
+            //checks and identifies for duplicates
+            //otherwise move to next node
+            if (curr.val == curr.next.val) {
+
+                //skips the nodes with the same value
+                while (curr.next != null && curr.val == curr.next.val) {
+                    curr = curr.next;
+                }
+
+                //changes prev node reference to remove duplicates from ll
+                prev.next = curr.next;
+            } else {
+                prev = prev.next;
+            }
+
+            curr = curr.next;
+        }
+
+        return dummy.next;
     }
 }
