da6a44302d9a9f767823131e11cba579c57bf24b
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-30 02:27:07 -0400

    This code implementation passes ALL test cases for the Rotate List problem with v1 solution

--- Top Interview 150/08. Linked List/08. Remove Duplicates from Sorted List II/v2/Rotate List/src/Solution.java
+++ Top Interview 150/08. Linked List/08. Remove Duplicates from Sorted List II/v2/Rotate List/src/Solution.java
@@ -1,3 +1,6 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -11,6 +14,40 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
 
-        return null;
+        //creates and initializes variables
+        ListNode dummy = new ListNode(0, head);
+        ListNode curr = head;
+        Deque<ListNode> nodeOrder = new ArrayDeque<ListNode>();
+        int size = 0;
+        int rotateMod = 0;
+
+        //find the size of the ll
+        while(curr != null) {
+            ++size;
+            nodeOrder.addLast(curr);
+            curr = curr.next;
+        }
+
+        //efficient way to rotate ll
+        if(size != 0)
+            rotateMod = k % size;
+
+        //rotate ll to the right
+        while(rotateMod > 0) {
+
+            ListNode lastNode = nodeOrder.removeLast();
+            ListNode next = dummy.next;
+
+            dummy.next = lastNode;
+            lastNode.next = next;
+
+            nodeOrder.peekLast().next = null;
+
+            nodeOrder.addFirst(lastNode);
+
+            --rotateMod;
+        }
+
+        return dummy.next;
     }
 }
