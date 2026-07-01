292ddf230962d2ec8426c0eb31ed77a0d6a58a7e
Gurjot Saini <gurjot.saini129@gmail.com>
2026-06-30 22:43:03 -0400

    This code implementation passes ALL test cases for the Rotate List problem with v2 solution

--- Top Interview 150/08. Linked List/09. Rotate List/v2/Rotate List/src/Solution.java
+++ Top Interview 150/08. Linked List/09. Rotate List/v2/Rotate List/src/Solution.java
@@ -11,6 +11,34 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
 
-        return null;
+        if(head == null) return head;
+
+        //creates and initializes variables
+        ListNode tail = head;
+        int size = 1;
+
+        //find the size of the ll
+        while(tail.next != null) {
+            ++size;
+            tail = tail.next;
+        }
+
+        //calculates the number of rotations using mod,
+        //which could significantly reduce the number of rotations
+        k = k % size;
+
+        if(k == 0) return head;
+
+        ListNode curr = head;
+
+        //find where to detach the ll and move it to the front as the new head
+        for(int i = 0; i < size - k - 1; ++i)
+            curr = curr.next;
+
+        ListNode newHead = curr.next; //stores the start part of the split that you want to move to front
+        curr.next = null; // this represent the end of the ll after the rotation
+        tail.next = head; //the represents the end part of the split, it will be connected to the beginning part of the ll
+
+        return newHead;
     }
 }
